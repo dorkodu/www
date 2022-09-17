540c603c4d08ae38b1941a1162f70eae4a3a43ea
doruk eray <doruk@dorkodu.com>
2022-09-17 21:56:11 +0300

    small updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# www
+# dorkodu's website
 
-public source code of dorkodu's website.
+public source code for [dorkodu.com].
 this is a fork of @dorkodu/prism nuxt3 website template.
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// prettier-ignore
 export default defineNuxtConfig({
-
+  alias: {
+    'images': 'public/images',
+    'scss': 'assets/scss',
+    'styles': 'assets/scss/styles.scss',
+  },
+  ssr: false,
+  target: 'static', // default is 'server'
+  css: [
+    "@/assets/scss/styles.scss"
+  ],
+  typescript: {
+    shim: false
+  },
+  modules: [
+    'nuxt-icon'
+  ]
 })
