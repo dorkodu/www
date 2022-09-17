74095558b46aad86986787933b98cc9df7ac65f3
doruk eray <doruk@dorkodu.com>
2022-09-17 19:27:37 +0300

    update nuxt: 3.0.0-rc.10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,4 @@
-import { defineNuxtConfig } from "nuxt";
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-
-// prettier-ignore
 export default defineNuxtConfig({
-  alias: {
-    'images': 'public/images',
-    'scss': 'assets/scss',
-    'styles': 'assets/scss/styles.scss',
-  },
-  ssr: false,
-  target: 'static', // default is 'server'
-  css: [
-    "@/assets/scss/styles.scss"
-  ],
-  typescript: {
-    shim: false
-  },
-  modules: [
-    'nuxt-icon'
-  ]
+
 })
