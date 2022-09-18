98d4b513880df55792739fe803cba3a47cc75dc5
doruk eray <doruk@dorkodu.com>
2022-09-18 17:37:59 +0300

    add favicons

--- app.vue
+++ app.vue
@@ -37,6 +37,17 @@ useHead({
 </script>
   
 <template>
+  <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2" />
+  <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2" />
+  <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2" />
+  <Link rel="manifest" href="/site.webmanifest" />
+  <Link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#20df30" />
+  <Link rel="shortcut icon" href="/favicon.ico?v=2" />
+  <Meta name="msapplication-TileColor" content="#20df30" />
+  <Meta name="msapplication-TileImage" content="/mstile-144x144.png?v=2" />
+  <Meta name="msapplication-config" content="/browserconfig.xml?v=2" />
+  <Meta name="theme-color" content="#20df30" />
+
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
