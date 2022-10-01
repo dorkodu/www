cf0c6ebe2ec485a2e929037bb2d6217f9021d6ed
doruk eray <doruk@dorkodu.com>
2022-10-01 23:29:40 +0300

    theme template

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,47 @@
 import { Theme } from "@emotion/react";
-import { system, reference, hsl } from "./prism";
+import { tokens, color } from "./prism";
+import fonts from "./fonts";
+import typography from "./typography";
 
-export const LightTheme: Theme = {
-  primary: 
+//? @use fonts
+//? @use colors
+//? @use tokens
+
+export const light: Theme = {
+  color: {
+    primary: "",
+    onPrimary: "",
+    primaryContainer: "",
+    onPrimaryContainer: "",
+
+    secondary: "",
+    onSecondary: "",
+    secondaryContainer: "",
+    onSecondaryContainer: "",
+
+    error: "",
+    onError: "",
+    errorContainer: "",
+    onErrorContainer: "",
+
+    background: "",
+    onBackground: "",
+
+    surface: "",
+    onSurface: "",
+
+    surfaceVariant: "",
+    onSurfaceVariant: "",
+
+    outline: "",
+
+    inverseOnSurface: "",
+    inverseSurface: "",
+    inversePrimary: "",
+
+    shadow: "",
+
+    surfaceTint: "",
+    surfaceTintColor: "",
+  },
 };
