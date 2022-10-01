a0b7d69e03a481eeeac4a088dcd869eabf73b56f
doruk eray <doruk@dorkodu.com>
2022-10-01 23:29:13 +0300

    update Theme interface for emotion

--- types/emotion.d.ts
+++ types/emotion.d.ts
@@ -2,6 +2,41 @@ import "@emotion/react";
 
 declare module "@emotion/react" {
   export interface Theme {
-    color: {};
+    color: {
+      primary: string;
+      onPrimary: string;
+      primaryContainer: string;
+      onPrimaryContainer: string;
+
+      secondary: string;
+      onSecondary: string;
+      secondaryContainer: string;
+      onSecondaryContainer: string;
+
+      error: string;
+      onError: string;
+      errorContainer: string;
+      onErrorContainer: string;
+
+      background: string;
+      onBackground: string;
+
+      surface: string;
+      onSurface: string;
+
+      surfaceVariant: string;
+      onSurfaceVariant: string;
+
+      outline: string;
+
+      inverseOnSurface: string;
+      inverseSurface: string;
+      inversePrimary: string;
+
+      shadow: string;
+
+      surfaceTint: string;
+      surfaceTintColor: string;
+    };
   }
 }
