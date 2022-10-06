dd2b0b4a73d24fec233686c1b4eec1fa66e5c53d
doruk eray <doruk@dorkodu.com>
2022-10-06 17:44:12 +0300

    update pages

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
