251e653a8f8b3dd179ca5c522e4425897e6d494b
doruk eray <doruk@dorkodu.com>
2022-10-06 23:29:02 +0300

    add type-checking to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
