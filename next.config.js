aa9d2d2660726a2db7ebe1be4fb8329e958777e5
doruk eray <doruk@dorkodu.com>
2022-10-06 17:42:53 +0300

    add i18n to next config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 };
 
 module.exports = nextConfig;
