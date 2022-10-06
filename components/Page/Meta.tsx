2317625dd6895ce0e3208babdca3307bfd3cefad
doruk eray <doruk@dorkodu.com>
2022-10-06 18:50:41 +0300

    update component: Meta

--- components/Page/Meta.tsx
+++ components/Page/Meta.tsx
@@ -1,82 +1,83 @@
 import { FunctionComponent } from "react";
+import website from "./website";
 
 interface MetaProps {
   title: string;
-  description?: string;
-  keywords?: string;
-  image?: string;
+  url: string;
+  description: string;
+  keywords: string;
+  image?: {
+    src: string;
+    alt: string;
+  };
 }
-
-<script setup>
-const route = useRoute();
-
-const WEBSITE_ROOT = "https://dorkodu.com";
-
-useHead({
-meta: [
-  { name: 'title', content: `${route.meta.title}` },
-  { name: 'description', content: `${route.meta.description}` },
-  { name: 'keywords', content: `${route.meta.keywords}` },
-  // open graph meta tags
-  { name: 'og:type', content: `website` },
-  { name: 'og:url', content: `${WEBSITE_ROOT + route.fullPath}` },
-  { name: 'og:title', content: `${route.meta.title}` },
-  { name: 'og:description', content: `${route.meta.description}` },
-
-  // twitter meta tags
-  { name: 'twitter:card', content: `summary` },
-  { name: 'twitter:url', content: `${WEBSITE_ROOT + route.fullPath}` },
-  { name: 'twitter:title', content: `${route.meta.title}` },
-  { name: 'twitter:description', content: `${route.meta.description}` },
-],
-bodyAttrs: {
-  class: 'body'
-},
-htmlAttrs: {
-  lang: "en"
-},
-script: [],
-})
-</script>
-  
-<template>
-  
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-</template>
-  
-<style lang="scss">
-@import "scss/styles.scss";
-</style>
-  
 
 const Meta: FunctionComponent<MetaProps> = ({
   title,
   description,
+  url,
   keywords,
   image,
 }) => {
   return (
-  <>
-    <title>{title}</title>
-    <meta name="keywords" content={keywords} />
-    <meta name="description" content={description} />
-    
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2" />
-    <link rel="manifest" href="/site.webmanifest" />
-    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#20df30" />
-    <link rel="shortcut icon" href="/favicon.ico?v=2" />
-    <meta name="msapplication-TileColor" content="#20df30" />
-    <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=2" />
-    <meta name="msapplication-config" content="/browserconfig.xml?v=2" />
-    <meta name="theme-color" content="#20df30" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5" />
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-    <meta charSet="UTF-8" />
-  </>
+    <>
+      <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=5"
+      />
+      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      <meta charSet="UTF-8" />
+
+      <meta name="og:type" content="website" />
+      <meta name="og:url" content={url} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content={website.twitter.username} />
+      <meta name="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+
+      {image && (
+        <>
+          <meta name="twitter:image" content={image.src} />
+          <meta name="twitter:image:alt" content={image.alt} />
+          <meta name="og:image" content={image.src} />
+          <meta name="og:image:alt" content={image.alt} />
+        </>
+      )}
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png?v=2"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png?v=2"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png?v=2"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#20df30" />
+      <link rel="shortcut icon" href="/favicon.ico?v=2" />
+      <meta name="msapplication-TileColor" content="#20df30" />
+      <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=2" />
+      <meta name="msapplication-config" content="/browserconfig.xml?v=2" />
+      <meta name="theme-color" content="#20df30" />
+    </>
   );
 };
 
