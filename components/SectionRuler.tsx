ff37e66ee58329aef1243988f4312d97a6cf3a7d
doruk eray <doruk@dorkodu.com>
2022-10-07 23:31:44 +0300

    update component: SectionRuler

--- components/SectionRuler.tsx
+++ components/SectionRuler.tsx
@@ -1,12 +1,15 @@
 import styled from "@emotion/styled";
-import { tokens } from "../styles/prism";
+import { FunctionComponent } from "react";
+import { tokens, ui } from "../styles/prism";
 
-export default function SectionRuler() {
+const SectionRuler: FunctionComponent = () => {
   return <Ruler></Ruler>;
-}
+};
+
+export default SectionRuler;
 
 const Ruler = styled.div`
   padding-bottom: 1rem;
   margin-bottom: 1rem;
-  border-bottom: 0.25rem dashed ${tokens.color.gray(90)};
+  border-bottom: 0.25rem dashed ${ui.color.neutral(90)};
 `;
