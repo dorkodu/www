4a2b58445fe0fe49952522ff3779b133c9c9b7e2
doruk eray <doruk@dorkodu.com>
2022-10-07 23:51:53 +0300

    rename layout (not sure)

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,20 @@
 import { ThemeProvider } from "@emotion/react";
 import styled from "@emotion/styled";
 import { light } from "../styles/theme";
+import Page from "./Page";
 
+interface LayoutProps {
+  children: React.ReactNode;
+}
 
-export default function Layout({ children }) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Page.Header />
       <ThemeProvider theme={light}>
         <main>{children}</main>
       </ThemeProvider>
+      <Page.Footer />
     </>
   );
 }
