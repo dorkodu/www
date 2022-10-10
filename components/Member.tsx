7582b6f4e6cbd0d1bede794dd1cf4e2073e52fde
doruk eray <doruk@dorkodu.com>
2022-10-10 16:46:42 +0300

    migrate component: Member

--- components/Member.tsx
+++ components/Member.tsx
@@ -1,63 +1,79 @@
-<template>
-  <div>
-    <div class="TeamMember">
-      <div class="__container">
-        <img v-if="avatar" class="avatar" :src="avatar" :alt="name">
-        <div class="__info">
-          <div class="icons">
-            <slot name="icons"></slot>
-          </div>
-          <p v-if="name" class="name">{{ name }}</p>
-          <p v-if="title" class="title">{{ title }}</p>
-          <p v-if="about" class="about">{{ about }}</p>
-          <div class="__more">
-            <slot name="more"></slot>
-          </div>
+import { css } from "@emotion/react";
+import { FunctionComponent } from "react";
+import { ui } from "../styles/prism";
+
+interface MemberProps {
+  name?: string;
+  title?: string;
+  about?: string;
+  avatar?: string;
+  icons?: React.ReactNode;
+  children: React.ReactNode;
+}
+
+const Member: FunctionComponent<MemberProps> = ({
+  name,
+  title,
+  about,
+  avatar,
+  icons,
+  children,
+}) => {
+  return (
+    <div css={style.root}>
+      <div css={style.container}>
+        {avatar && <img css={style.avatar} src={avatar} alt={name} />}
+        <div css={style.info}>
+          {icons && <div css={style.icons}>{icons}</div>}
+          {name && <p css={style.name}>{name}</p>}
+          {title && <p css={style.title}>{title}</p>}
+          {about && <p css={style.about}>{about}</p>}
+          <div>{children}</div>
         </div>
       </div>
     </div>
-  </div>
-</template>
+  );
+};
 
-<script setup lang="ts">
-const { name, title, about, avatar }
-  = defineProps<{
-    name?: string;
-    title?: string;
-    about?: string;
-    avatar?: string;
-  }>();
-</script>
+export default Member;
 
-<style lang="scss" scoped>
-@import "styles";
-
-.TeamMember {
-  display: flex;
-  padding: 1rem;
-  justify-content: center;
-
-  .__container {
+const style = {
+  root: css`
+    display: flex;
+    padding: 1rem;
+    justify-content: center;
+  `,
+  container: css`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap: .5rem;
+    gap: 0.5rem;
     height: 100%;
     max-width: 28rem;
-  }
 
-  .avatar {
+    @media (min-width: 640px) {
+      text-align: left;
+      flex-direction: row;
+      justify-content: flex-start;
+      gap: 1rem;
+      max-width: 36rem;
+    }
+  `,
+  avatar: css`
     object-fit: cover;
     object-position: center;
     align-self: flex-start;
     flex-shrink: 0;
     width: 7.5rem;
     height: 7.5rem;
-    border-radius: $system-layout-curve;
-  }
+    border-radius: ${ui.layout.curve};
 
-  .__info {
+    @media (min-width: 640px) {
+      margin: 0;
+    }
+  `,
+  info: css`
     display: flex;
     flex-direction: column;
     flex-grow: 1;
@@ -66,52 +82,30 @@ const { name, title, about, avatar }
       padding: 0;
       margin: 0;
     }
-  }
-
-  .icons {
+  `,
+  icons: css`
     display: flex;
-    color: $tokens.color.gray(70;
-    margin-bottom: .15rem;
+    color: ${ui.color.neutral(70)};
+    margin-bottom: 0.15rem;
 
     * {
       stroke-width: 3;
     }
-  }
-
-  .name {
+  `,
+  name: css`
     font-size: 1.35rem;
     font-weight: 700;
-  }
-
-  .title {
-    margin-bottom: .1rem !important;
-    color: $tokens.color.gray(60;
+  `,
+  title: css`
+    margin-bottom: 0.1rem !important;
+    color: ${ui.color.neutral(60)};
     font-size: 1.25rem;
     font-weight: 600;
-  }
-
-  .about {
+  `,
+  about: css`
     font-size: 1.15rem;
     line-height: 1.35rem;
     font-weight: 400;
-    color: $tokens.color.gray(20;
-  }
-}
-
-@media (min-width: 640px) {
-  .TeamMember {
-
-    .__container {
-      text-align: left;
-      flex-direction: row;
-      justify-content: flex-start;
-      gap: 1rem;
-      max-width: 36rem;
-    }
-
-    .avatar {
-      margin: 0;
-    }
-  }
-}
-</style>
+    color: ${ui.color.neutral(20)};
+  `,
+};
