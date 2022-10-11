5bca3635715f545d3dc78e5b984b4845f01b6278
doruk eray <doruk@dorkodu.com>
2022-10-11 16:18:08 +0300

    migrate page: Oops

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
--- pages/oops.tsx
+++ pages/oops.tsx
@@ -1,40 +1,51 @@
-<script setup lang="ts">
-const page = {
-  title: "oops!",
-  tagline: "something's gone wrong :(",
-  statement: `you shouldn't be here. you can use the links to surf the website, or just go back to the home page.`,
-  picture: {
-    source: "/images/zombie.png",
+import { css } from "@emotion/react";
+import type { NextPage } from "next";
+import Image from "next/image";
+import Button from "../components/Button";
+import Hero from "../components/Hero";
+import Polaroid from "../components/Polaroid";
+import { ButtonContainer, Showcase } from "../styles/blame";
+import { tokens, ui } from "../styles/prism";
+import Page from "../components/Page";
+import PlaceholderImage from "../public/images/workdesk.jpg";
+
+const meta = {
+  title: "oops! — dorkodu",
+  description: "something's gone wrong.",
+  keywords: "404, oops, not found",
+  url: "/oops",
+  image: {
+    src: "/public/images/dorkodu-single.png",
+    alt: "dorkodu logo with single letter.",
   },
 };
 
-definePageMeta({
-  title: "oops!",
-  description: "something's gone wrong :(",
-  keywords: "oops, 404, not found",
-});
-</script>
+const image = {
+  intro: PlaceholderImage,
+};
 
-<template>
-  <main>
-    <Showcase style="margin-top: 0;">
-      <template #main>
-        <Hero :title="page.title" :tagline="page.tagline" :statement="page.statement" style="margin-top: 0 !important;">
-          <template #head>
-          </template>
-          <template #content>
-            <div class="button-container" style="padding: 0;">
-              <Button type="fill" link="/">back to home ↗</Button>
-            </div>
-          </template>
+const Oops: NextPage = () => {
+  return (
+    <>
+      <Page.Meta {...meta} />
+
+      <Showcase>
+        <Hero
+          title="oops!"
+          tagline="something's gone wrong."
+          statement="you shouldn't be here. you can use the links to surf the website, or just go back to the home page."
+        >
+          <ButtonContainer>
+            <Button type="fill" link="/">
+              back to home ↗
+            </Button>
+          </ButtonContainer>
         </Hero>
-      </template>
-      <template #side>
-        <Picture :source="page.picture.source" />
-      </template>
-    </Showcase>
-    <Break />
+        <Polaroid source={image.intro} />
+      </Showcase>
+      <br />
+    </>
+  );
+};
 
-    <PageLegend />
-  </main>
-</template>
+export default Oops;
