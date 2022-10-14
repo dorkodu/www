0f1573aff591ce9d5d9b575c30763ba42cc6dfa4
doruk eray <doruk@dorkodu.com>
2022-10-15 00:04:29 +0300

    new components: Work.Entry, Work.Role

---
+++ components/Work/Role.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+import { WorkRole } from "../../types/dorkodu";
+
+const Role: FunctionComponent<{
+  role: WorkRole;
+}> = ({ role }) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Role;
