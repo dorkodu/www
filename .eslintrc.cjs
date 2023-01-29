eea426a963bfc134366dee102ac00e189e0140ad
doruk eray <doruk@dorkodu.com>
2023-01-29 20:39:49 +0300

    configure eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    indent: [
+      "warn",
+      2,
+      {
+        SwitchCase: 1,
+        MemberExpression: 1,
+        StaticBlock: { body: 1 },
+        ArrayExpression: 1,
+        ObjectExpression: 1,
+        ImportDeclaration: 1,
+        flatTernaryExpressions: false,
+        offsetTernaryExpressions: false,
+        ignoreComments: true,
+      },
+    ],
+    "linebreak-style": ["warn", "unix"],
+    quotes: ["warn", "double"],
+    semi: ["warn", "always"],
+  },
+};
