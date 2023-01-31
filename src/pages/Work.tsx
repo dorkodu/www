dc685799fb09c5b13e1c0b2ac78bb2bb0a45e0cb
doruk eray <doruk@dorkodu.com>
2023-01-31 23:59:34 +0300

    new page: Work

---
+++ src/pages/Work.tsx
@@ -0,0 +1,8 @@
+import { Container } from "@mantine/core";
+import { FunctionComponent } from "react";
+
+const Work: FunctionComponent = () => {
+  return <Container size={1200} py={20}></Container>;
+};
+
+export default Work;
