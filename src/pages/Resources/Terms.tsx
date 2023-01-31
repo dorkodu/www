86ce69ca3080861f530ada3d1aa08cd2ab1fc695
doruk eray <doruk@dorkodu.com>
2023-01-31 23:59:38 +0300

    new page: Terms

---
+++ src/pages/Resources/Terms.tsx
@@ -0,0 +1,8 @@
+import { Container } from "@mantine/core";
+import { FunctionComponent } from "react";
+
+const Terms: FunctionComponent = () => {
+  return <Container size={1200} py={20}></Container>;
+};
+
+export default Terms;
