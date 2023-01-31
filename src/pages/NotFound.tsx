e417aa0d657a68561cf579f874e10e5b53dc00dc
doruk eray <doruk@dorkodu.com>
2023-01-31 23:59:56 +0300

    new page: NotFound

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+import { Container } from "@mantine/core";
+import { FunctionComponent } from "react";
+
+const NotFound: FunctionComponent = () => {
+  return <Container size={1200} py={20}></Container>;
+};
+
+export default NotFound;
