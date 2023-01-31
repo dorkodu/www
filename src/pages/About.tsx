7aef542c6356df010988a0e8562a612872e8659c
doruk eray <doruk@dorkodu.com>
2023-02-01 00:00:00 +0300

    new page: About

---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+import { Container } from "@mantine/core";
+import { FunctionComponent } from "react";
+
+const About: FunctionComponent = () => {
+  return <Container size={1000} py={20}></Container>;
+};
+
+export default About;
