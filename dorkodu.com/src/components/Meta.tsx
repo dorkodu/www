55052d4cff032c2fb8ffaba33eea4dfce64d5d4d
doruk eray <doruk@dorkodu.com>
2023-02-25 23:54:36 +0300

    update meta

--- dorkodu.com/src/components/Meta.tsx
+++ dorkodu.com/src/components/Meta.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
-import { website } from "../data/data";
+import { website } from "@/website";
 
 interface MetaProps {
   title: string;
@@ -12,13 +12,7 @@ interface MetaProps {
   };
 }
 
-const Meta: FunctionComponent<MetaProps> = ({
-  title,
-  description,
-  url,
-  keywords,
-  image,
-}) => {
+const Meta: FunctionComponent<MetaProps> = ({ title, description, url, keywords, image }) => {
   const URL = website.root + url;
 
   return (
@@ -28,16 +22,8 @@ const Meta: FunctionComponent<MetaProps> = ({
       <meta name="description" content={description} key="meta:description" />
       <meta name="keywords" content={keywords} key="meta:keywords" />
 
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=5"
-        key="meta:viewport"
-      />
-      <meta
-        httpEquiv="Content-Type"
-        content="text/html;charset=UTF-8"
-        key="meta:contentType"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5" key="meta:viewport" />
+      <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" key="meta:contentType" />
       <meta charSet="UTF-8" key="meta:charset" />
 
       <meta name="og:type" content="website" key="og:type" />
@@ -46,75 +32,29 @@ const Meta: FunctionComponent<MetaProps> = ({
       <meta name="og:description" content={description} key="og:description" />
 
       <meta name="twitter:card" content="summary" key="twitter:card" />
-      <meta
-        name="twitter:site"
-        content={website.twitter.username}
-        key="twitter:site"
-      />
+      <meta name="twitter:site" content={website.twitter.username} key="twitter:site" />
       <meta name="twitter:url" content={URL} key="twitter:url" />
       <meta name="twitter:title" content={title} key="twitter:title" />
-      <meta
-        name="twitter:description"
-        content={description}
-        key="twitter:description"
-      />
+      <meta name="twitter:description" content={description} key="twitter:description" />
 
       {image && (
         <>
           <meta name="twitter:image" content={image.src} key="twitter:image" />
-          <meta
-            name="twitter:image:alt"
-            content={image.alt}
-            key="twitter:image:alt"
-          />
+          <meta name="twitter:image:alt" content={image.alt} key="twitter:image:alt" />
           <meta name="og:image" content={image.src} key="og:image" />
           <meta name="og:image:alt" content={image.alt} key="og:image:alt" />
         </>
       )}
 
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png?v=2"
-        key="apple-touch-icon"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png?v=2"
-        key="icon:32"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png?v=2"
-        key="icon:16"
-      />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2" key="apple-touch-icon" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2" key="icon:32" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2" key="icon:16" />
       <link rel="manifest" href="/site.webmanifest" key="manifest" />
-      <link
-        rel="mask-icon"
-        href="/safari-pinned-tab.svg?v=2"
-        color="#20df30"
-        key="mask-icon"
-      />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#20df30" key="mask-icon" />
       <link rel="shortcut icon" href="/favicon.ico?v=2" key="shortcut icon" />
-      <meta
-        name="msapplication-TileColor"
-        content="#20df30"
-        key="msapplication-TileColor"
-      />
-      <meta
-        name="msapplication-TileImage"
-        content="/mstile-144x144.png?v=2"
-        key="msapplication-TileImage"
-      />
-      <meta
-        name="msapplication-config"
-        content="/browserconfig.xml?v=2"
-        key="msapplication-config"
-      />
+      <meta name="msapplication-TileColor" content="#20df30" key="msapplication-TileColor" />
+      <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=2" key="msapplication-TileImage" />
+      <meta name="msapplication-config" content="/browserconfig.xml?v=2" key="msapplication-config" />
       <meta name="theme-color" content="#20df30" key="theme-color" />
     </>
   );
