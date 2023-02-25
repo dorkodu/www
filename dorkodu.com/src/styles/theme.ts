171497bf05f93b7b96b2e2555d519e758b9c1471
doruk eray <doruk@dorkodu.com>
2023-02-25 23:54:27 +0300

    update theme

--- dorkodu.com/src/styles/theme.ts
+++ dorkodu.com/src/styles/theme.ts
@@ -103,18 +103,14 @@ const theme: MantineThemeOverride = {
   focusRingStyles: {
     styles: (theme: MantineTheme) => ({
       outlineOffset: 2,
-      outline: `2px solid ${
-        theme.colors["blue"][theme.colorScheme === "dark" ? 7 : 5]
-      }`,
+      outline: `2px solid ${theme.colors["blue"][theme.colorScheme === "dark" ? 7 : 5]}`,
     }),
     resetStyles: () => ({ outline: "none" }),
     inputStyles: (theme: MantineTheme) => ({
       outline: "none",
       borderColor:
         theme.colors["blue"][
-          typeof theme.primaryShade === "object"
-            ? theme.primaryShade[theme.colorScheme]
-            : theme.primaryShade
+          typeof theme.primaryShade === "object" ? theme.primaryShade[theme.colorScheme] : theme.primaryShade
         ],
     }),
   },
