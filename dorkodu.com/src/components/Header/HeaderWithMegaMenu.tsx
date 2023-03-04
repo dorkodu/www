af98c3850864d02d64cff8c7a3dc8f6558f7d950
doruk eray <doruk@dorkodu.com>
2023-03-05 02:30:45 +0300

    updates to Header

--- dorkodu.com/src/components/Header/HeaderWithMegaMenu.tsx
+++ dorkodu.com/src/components/Header/HeaderWithMegaMenu.tsx
@@ -20,7 +20,23 @@ import {
   ThemeIcon,
 } from "@mantine/core";
 
-import { IconBriefcase, IconBriefcaseOff, IconCampfire, IconChevronDown } from "@tabler/icons-react";
+import {
+  IconArrowBigRightLine,
+  IconArrowBigRightLineFilled,
+  IconBriefcase,
+  IconBriefcaseOff,
+  IconBuildingCommunity,
+  IconBusinessplan,
+  IconCake,
+  IconCampfire,
+  IconChevronDown,
+  IconCircleArrowRightFilled,
+  IconHeartHandshake,
+  IconLicense,
+  IconMasksTheater,
+  IconSquareRoundedArrowRight,
+  IconSquareRoundedArrowRightFilled,
+} from "@tabler/icons-react";
 
 import { useDisclosure } from "@mantine/hooks";
 import { FunctionComponent } from "react";
@@ -61,12 +77,11 @@ const useStyles = createStyles((theme) => ({
 
   subLink: {
     width: "100%",
-    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+    padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
     borderRadius: theme.radius.md,
 
     ...theme.fn.hover({
       backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[1],
-      textDecoration: "underline",
     }),
 
     "&:active": theme.activeStyles,
@@ -94,15 +109,10 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-export const HeaderWithMegaMenu: FunctionComponent = () => {
-  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-
-  const { classes, theme } = useStyles();
-
-  const productLinks = [
+const links = {
+  product: [
     {
-      icon: () => (
+      icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width={36} height={36} viewBox="0 0 36 36">
           <path
             fill="#A6D388"
@@ -122,7 +132,7 @@ export const HeaderWithMegaMenu: FunctionComponent = () => {
       description: "Mind Garden",
     },
     {
-      icon: () => (
+      icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width={36} height={36} viewBox="0 0 36 36">
           <path fill="#269" d="M36 32a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h28a4 4 0 0 1 4 4v28z"></path>
           <path fill="#F4900C" d="M11 9s7.29-4.557 21-4.969c.652-.02 2-.031 2 .969c-6 9-9 23-9 23L11 9z"></path>
@@ -137,7 +147,7 @@ export const HeaderWithMegaMenu: FunctionComponent = () => {
       description: "Social & Gamified Life Growth",
     },
     {
-      icon: () => (
+      icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width={36} height={36} viewBox="0 0 36 36">
           <path fill="#292F33" d="M7 11h22v18H7z"></path>
           <path
@@ -169,7 +179,7 @@ export const HeaderWithMegaMenu: FunctionComponent = () => {
       description: "Public Town Square",
     },
     {
-      icon: () => (
+      icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width={36} height={36} viewBox="0 0 36 36">
           <path
             fill="#AAB8C2"
@@ -185,158 +195,69 @@ export const HeaderWithMegaMenu: FunctionComponent = () => {
       title: "ID",
       description: "Digital Life Hub",
     },
-  ];
-
-  const workLinks1 = [
+  ],
+  company: [
     {
-      icon: () => (
-        <svg xmlns="http://www.w3.org/2000/svg" width={36} height={36} viewBox="0 0 36 36">
-          <path
-            fill="#5C913B"
-            d="M35.602 16.28c-2.582-3.761-7.92 1.774-17.662 2.899c-3.607.416-6.857 3.428-11.047 3.141c-1.718-.118-4.117-5.061-5.913-4.273c-.924.406-1.348 1.761-.398 2.779L11.13 33.412s.536.618 2.342.19c.445.131 7.135-2.55 11.003-7.917c.926-1.285 2.03-2.355 3.173-3.249c3.711-2.744 7.745-3.7 7.745-3.7c.516-.426.637-.879.609-1.272c.034-.656-.4-1.184-.4-1.184z"
-          ></path>
-          <path
-            fill="#A7D28B"
-            d="M11.476 10.274c-3.783 5.25-10.408 7.737-10.408 7.737c-1.236 1.047-.192 2.281-.192 2.281l10.438 12.359s.522.617 2.28.189c0 0 6.625-2.482 10.407-7.732c3.899-5.41 10.93-7.118 10.93-7.118c1.235-1.047.192-2.283.192-2.283L24.685 3.35s-.522-.618-2.28-.192c0 0-7.03 1.704-10.929 7.116z"
-          ></path>
-          <circle cx="22" cy="14" r="5.5" fill="#77B255"></circle>
-          <path
-            fill="#5C913B"
-            d="M12.873 31.929c-.881 0-1.204-.328-1.248-.378L2.216 20.568c-.018-.021-.495-.567-.437-1.261c.035-.421.253-.796.647-1.115l.063-.038c.061-.024 6.163-2.532 9.583-7.065l.2.15l-.2-.15c3.618-4.796 9.859-6.854 9.921-6.874c1.745-.406 2.316.174 2.377.242l9.285 11.044c.017.018.493.566.435 1.261c-.035.42-.253.795-.647 1.114l-.099.049c-.061.015-6.129 1.523-9.644 6.181c-3.499 4.64-9.607 7.642-9.668 7.671c-.464.11-.846.152-1.159.152zm-.871-.707c.001 0 .452.416 1.865.088c.008-.011 6.009-2.962 9.436-7.504c3.437-4.555 9.225-6.182 9.867-6.351c.257-.221.399-.466.422-.729c.042-.48-.312-.893-.315-.897L23.992 4.784c-.002 0-.45-.415-1.864-.087c-.041.014-6.135 2.026-9.656 6.693c-3.366 4.461-9.239 6.995-9.758 7.213c-.268.225-.414.476-.437.745c-.04.476.312.887.315.891l9.41 10.983z"
-          ></path>
-          <path
-            fill="#FFAC33"
-            d="m21.276 22.166l-.006 5.94l.497.59c.969-.884 1.9-1.878 2.705-2.996a15.844 15.844 0 0 1 2.828-2.986l-.483-.575l-5.541.027z"
-          ></path>
-          <path
-            fill="#FFE8B6"
-            d="M26.798 22.118L14.292 7.305c-1.016.836-1.992 1.811-2.822 2.964c-.811 1.126-1.755 2.117-2.735 2.991l1.462 1.731l-.001.001l11.075 13.114a18.71 18.71 0 0 0 2.725-3.003c.83-1.152 1.805-2.126 2.82-2.962l-.019-.023h.001z"
-          ></path>
-          <path
-            fill="#5C913B"
-            d="M14.479 22.555c-1.557-1.844-4.853 1.148-5.793.035c-.455-.539-.162-1.238.436-1.742c1.005-.85 1.73-.355 2.185-.74c.323-.272.306-.605.114-.834c-.446-.527-1.586-.252-2.472.26l-.431-.51a.754.754 0 0 0-1.152.972l.446.529c-.677.898-.907 2.09-.106 3.037c1.496 1.77 4.833-1.172 5.883.072c.364.432.262 1.256-.504 1.902c-1.148.971-2.188.516-2.655.91c-.228.191-.269.555-.026.844c.387.457 1.62.359 2.805-.379c.002.002.002.004.002.006l.487.576a.755.755 0 1 0 1.153-.974l-.487-.576c-.005-.006-.013-.01-.019-.016c.799-.978 1.069-2.267.134-3.372z"
-          ></path>
-          <path
-            fill="#E1E8ED"
-            d="M32.456 32.588c.009-.008.019-.014.028-.021c.068-.068.121-.146.181-.221c.042-.052.089-.102.128-.155c.035-.05.064-.101.096-.152c1.42-2.187.49-5.895-2.321-8.707c-2.812-2.812-6.521-3.742-8.707-2.321c-.052.031-.103.06-.153.096c-.053.039-.104.086-.154.127c-.074.061-.152.113-.221.182c-.009.009-.015.019-.022.028c-.011.011-.024.018-.036.03l.006.005c-1 1.062-1.012 2.705-.006 3.712c1.008 1.008 2.65.994 3.713-.006l.011.012c-.021.02-.044.033-.064.053c-1.059 1.059-1.084 2.748-.059 3.775c1.026 1.025 2.717 1 3.775-.059c.02-.02.033-.043.053-.064l.011.012c-1 1.062-1.013 2.705-.005 3.713c1.007 1.006 2.649.994 3.711-.006l.006.006c.01-.014.018-.027.029-.039z"
-          ></path>
-          <path
-            fill="#CCD6DD"
-            d="M21.277 25.231c.906.905 2.321.979 3.37.261c.439-.75.565-1.463.565-1.686c-2.207 1.646-4.177.009-4.618-.97c-.183.846.039 1.75.683 2.395zm7.299 3.84c.298-.419.529-.824.637-1.098c-1.405 1.288-4.091.345-4.905-1.698c-.358.95-.175 2.044.571 2.79c1.002 1.002 2.634.994 3.697.006zm3.854 3.595l.029-.036c.01-.008.02-.014.028-.022c.069-.068.122-.146.181-.22c.043-.053.089-.102.128-.156c.036-.049.065-.101.097-.152c.065-.1.1-.218.155-.324c-2.528 1.131-4.415-.788-4.944-1.745c-.295.917-.103 1.944.609 2.657c1.006 1.007 2.648.995 3.711-.006l.006.004z"
-          ></path>
-          <path
-            fill="#E1E8ED"
-            d="m13.956 12.498l.028-.022c.069-.068.122-.146.181-.22c.042-.052.088-.101.127-.155c.036-.05.065-.101.097-.152c1.42-2.187.489-5.896-2.322-8.707C9.255.43 5.547-.501 3.36.919c-.051.032-.103.061-.153.097c-.054.039-.103.085-.155.127c-.074.06-.152.112-.22.181c-.009.009-.015.02-.022.028c-.011.01-.025.019-.036.03l.005.005c-1 1.062-1.012 2.705-.005 3.712c1.007 1.007 2.65.995 3.712-.005l.011.011c-.021.019-.044.033-.064.053c-1.059 1.059-1.084 2.748-.058 3.775c1.026 1.027 2.716 1.001 3.775-.058c.02-.02.033-.043.053-.064l.011.011c-1 1.062-1.013 2.705-.005 3.712c1.007 1.007 2.649.995 3.711-.006l.005.006c.011-.011.02-.024.031-.036z"
-          ></path>
-          <path
-            fill="#CCD6DD"
-            d="M2.773 5.1c.906.906 2.321.98 3.37.26c.439-.75.564-1.462.564-1.685c-2.206 1.645-4.177.007-4.617-.972c-.183.847.039 1.752.683 2.397zm7.301 3.838c.297-.419.528-.822.635-1.096c-1.406 1.288-4.092.344-4.905-1.699c-.358.95-.175 2.044.57 2.79c1.004 1.003 2.637.994 3.7.005zm3.851 3.597c.011-.011.02-.024.03-.036l.028-.022c.069-.068.122-.146.181-.22c.042-.052.088-.101.127-.155c.036-.05.065-.101.097-.152c.065-.1.1-.219.155-.325c-2.528 1.131-4.415-.787-4.944-1.744c-.295.917-.103 1.944.609 2.656c1.007 1.007 2.649.995 3.711-.006l.006.004z"
-          ></path>
-        </svg>
-      ),
-      title: "Jobs",
-      description: "Join the revolution.",
-    },
-    {
-      icon: () => (
-        <svg xmlns="http://www.w3.org/2000/svg" width={36} height={36} viewBox="0 0 36 36">
-          <path
-            fill="#78B159"
-            d="M18 25.18C3.438 25.18 0 29.844 0 32s1.791 4 4 4h28c2.209 0 4-1.851 4-4s-3.438-6.82-18-6.82z"
-          ></path>
-          <path
-            fill="#662113"
-            d="M28.804 28.15c0 1.531-1.242 2.08-2.773 2.08c-1.531 0-2.773-.548-2.773-2.08l.693-6.239c0-1.531.548-1.387 2.08-1.387c1.531 0 2.08-.145 2.08 1.387l.693 6.239z"
-          ></path>
-          <path
-            fill="#5C913B"
-            d="M34.868 19.717c-4.59-4.135-6.449-10.056-8.837-10.056s-4.247 5.921-8.836 10.056c-1.929 1.738-1.01 2.743 1.634 2.542c2.357-.179 4.561-.741 7.202-.741s4.846.563 7.202.741c2.644.201 3.563-.805 1.635-2.542z"
-          ></path>
-          <path
-            fill="#3E721D"
-            d="M33.377 18.078c-3.527-3.401-5.509-8.273-7.346-8.273c-1.836 0-3.818 4.872-7.345 8.273c-2.803 2.703 3.673 3.309 7.345 0c3.671 3.309 10.148 2.703 7.346 0z"
-          ></path>
-          <path
-            fill="#5C913B"
-            d="M33.377 15.323c-3.527-3.178-5.509-7.729-7.346-7.729c-1.836 0-3.818 4.551-7.345 7.729c-2.803 2.525 3.673 3.091 7.345 0c3.671 3.091 10.148 2.524 7.346 0z"
-          ></path>
-          <path
-            fill="#3E721D"
-            d="M31.766 13.066c-2.753-2.481-4.301-6.034-5.735-6.034c-1.433 0-2.981 3.553-5.735 6.034c-2.188 1.971 2.867 2.414 5.735 0c2.867 2.413 7.923 1.971 5.735 0z"
-          ></path>
-          <path
-            fill="#5C913B"
-            d="M31.766 11.001c-2.753-2.481-4.301-6.034-5.735-6.034c-1.433 0-2.981 3.553-5.735 6.034c-2.188 1.971 2.867 2.414 5.735 0c2.867 2.414 7.923 1.972 5.735 0z"
-          ></path>
-          <path
-            fill="#3E721D"
-            d="M30.659 9.168c-2.222-2.002-3.471-4.869-4.628-4.869s-2.406 2.867-4.628 4.869c-1.766 1.591 2.314 1.948 4.628 0c2.314 1.948 6.395 1.591 4.628 0z"
-          ></path>
-          <path
-            fill="#5C913B"
-            d="M30.659 7.614c-2.222-2.002-3.471-4.869-4.628-4.869s-2.406 2.867-4.628 4.869c-1.766 1.591 2.314 1.948 4.628 0c2.314 1.948 6.395 1.591 4.628 0z"
-          ></path>
-          <path
-            fill="#3E721D"
-            d="M29.553 5.326c-1.69-1.523-2.641-3.705-3.522-3.705c-.88 0-1.831 2.182-3.522 3.705c-1.344 1.211 1.761 1.482 3.522 0c1.761 1.483 4.866 1.211 3.522 0z"
-          ></path>
-          <path
-            fill="#5C913B"
-            d="M29.553 4.29C27.863 2.767 26.912.585 26.031.585c-.88 0-1.831 2.182-3.522 3.705c-1.344 1.211 1.761 1.482 3.522 0c1.761 1.482 4.866 1.211 3.522 0z"
-          ></path>
-          <path
-            fill="#F4900C"
-            d="M15.101 33c-.424 0-.802-.267-.944-.666L9.145 18.301a.964.964 0 0 1 .125-.896a.99.99 0 0 1 .82-.405h11.759c.401 0 .764.22.922.589l6.014 14.023A.994.994 0 0 1 27.862 33H15.101z"
-          ></path>
-          <path
-            fill="#FFCC4D"
-            d="M4.075 33a1.005 1.005 0 0 1-.921-1.398L9.172 17.56a1.002 1.002 0 0 1 1.843 0l6.018 14.041A1.006 1.006 0 0 1 16.111 33H4.075z"
-          ></path>
-          <path fill="#292F33" d="M10.09 19.436L8.084 33h4.01z"></path>
-          <path fill="#FFAC33" d="M14.127 31.887L14.575 33h-2.481zm-8.075 0L5.604 33h2.48z"></path>
-          <path
-            fill="#FFE8B6"
-            d="M12.094 33s1.823-.465 2.714-1.288L10.09 19.436L12.094 33zm-4.01 0s-1.823-.465-2.714-1.288l4.719-12.277L8.084 33z"
-          ></path>
-          <path
-            fill="#E1E8ED"
-            d="M13.625 5.625c-.018 0-.034.006-.052.006a2.5 2.5 0 0 0-2.448-3.007a2.49 2.49 0 0 0-2.11 1.171a2.48 2.48 0 0 0-.89-.17a2.491 2.491 0 0 0-2.469 2.19a2.422 2.422 0 0 0-.906-.19a2.5 2.5 0 1 0 0 5h8.875a2.5 2.5 0 1 0 0-5z"
-          ></path>
-        </svg>
-      ),
-      title: "Life @ Dorkodu",
-      description: "Why work with us?",
-    },
-  ];
-
-  const workLinks = [
-    {
-      icon: () => (
+      icon: (
         <ThemeIcon variant="light" radius="md" size="lg" color="green">
-          <IconBriefcase />
+          <IconBriefcase stroke={2.25} />
         </ThemeIcon>
       ),
       title: "Jobs",
       description: "Join the revolution.",
     },
+
     {
-      icon: () => (
+      icon: (
         <ThemeIcon variant="light" radius="md" size="lg" color="green">
-          <IconBriefcase />
+          <IconLicense stroke={2.25} />
         </ThemeIcon>
       ),
-      title: "Life @ Dorkodu",
-      description: "Why work with us?",
+      title: "Our Manifesto",
+      description: "Liberate the humankind",
     },
-  ];
+  ],
+  business: [
+    {
+      icon: (
+        <ThemeIcon variant="light" radius="md" size="lg" color="green">
+          <IconBuildingCommunity stroke={2.25} />
+        </ThemeIcon>
+      ),
+      title: "For Business",
+      description: "Let's Grow Together!",
+    },
+    {
+      icon: (
+        <ThemeIcon variant="light" radius="md" size="lg" color="green">
+          <IconHeartHandshake stroke={2} />
+        </ThemeIcon>
+      ),
+      title: "For Creators",
+      description: "Let's Create Together!",
+    },
+  ],
+};
 
-  const links = {
-    products: productLinks.map((item) => (
+export const HeaderWithMegaMenu: FunctionComponent = () => {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+
+  const { classes, theme } = useStyles();
+
+  const MenuButtonsWithIcon = (
+    list: Array<{
+      icon: React.ReactNode;
+      title: string;
+      description: string;
+    }>
+  ) => {
+    return list.map((item) => (
       <UnstyledButton className={classes.subLink} key={item.title}>
         <Group noWrap align="flex-start">
-          <item.icon />
+          {item.icon}
           <div>
-            <Text size="sm" weight={500}>
+            <Text size="sm" weight={550}>
               {item.title}
             </Text>
             <Text size="xs" color="dimmed">
@@ -345,22 +266,7 @@ export const HeaderWithMegaMenu: FunctionComponent = () => {
           </div>
         </Group>
       </UnstyledButton>
-    )),
-    others: workLinks.map((item) => (
-      <UnstyledButton className={classes.subLink} key={item.title}>
-        <Group noWrap align="flex-start">
-          <item.icon />
-          <div>
-            <Text size="sm" weight={500}>
-              {item.title}
-            </Text>
-            <Text size="xs" color="dimmed">
-              {item.description}
-            </Text>
-          </div>
-        </Group>
-      </UnstyledButton>
-    )),
+    ));
   };
 
   return (
@@ -389,20 +295,20 @@ export const HeaderWithMegaMenu: FunctionComponent = () => {
                 <a href="#" className={classes.link}>
                   <Center inline>
                     <Box component="span" mr={5}>
-                      Work
+                      Our Work
                     </Box>
                     <IconChevronDown size={18} color={theme.colors.gray[5]} />
                   </Center>
                 </a>
               </HoverCard.Target>
 
-              <HoverCard.Dropdown sx={{ overflow: "hidden", marginTop: 16 }}>
+              <HoverCard.Dropdown sx={{ overflow: "hidden", marginTop: 16 }} p="sm">
                 <Group position="apart" px="md">
                   <Text weight={500}>Products</Text>
                 </Group>
                 <Divider my="sm" mx="-md" color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"} />
                 <SimpleGrid cols={2} spacing={0}>
-                  {links.products}
+                  {MenuButtonsWithIcon(links.product)}
                 </SimpleGrid>
 
                 <div className={classes.dropdownFooter}>
@@ -421,12 +327,12 @@ export const HeaderWithMegaMenu: FunctionComponent = () => {
               </HoverCard.Dropdown>
             </HoverCard>
 
-            <HoverCard width={225} position="bottom" radius="md" shadow="md" withinPortal>
+            <HoverCard width={250} position="bottom" radius="md" shadow="md" withinPortal>
               <HoverCard.Target>
                 <a href="#" className={classes.link}>
                   <Center inline>
                     <Box component="span" mr={5}>
-                      Business
+                      Company
                     </Box>
                     <IconChevronDown size={18} color={theme.colors.gray[5]} />
                   </Center>
@@ -434,15 +340,22 @@ export const HeaderWithMegaMenu: FunctionComponent = () => {
               </HoverCard.Target>
 
               <HoverCard.Dropdown sx={{ overflow: "hidden" }} p={8}>
-                <Stack spacing="xs">{links.others}</Stack>
+                <Stack spacing="xs">
+                  {MenuButtonsWithIcon(links.company)}
+                  <Divider color={theme.colorScheme === "dark" ? "dark.4" : "gray.2"} />
+                  {MenuButtonsWithIcon(links.business)}
+                </Stack>
               </HoverCard.Dropdown>
             </HoverCard>
           </Group>
 
           <Group className={classes.hiddenMobile} spacing="xs">
             <ColorToggleSwitch size="md" />
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
+            {/* <Button radius={theme.radius.lg - 2} rightIcon={<IconCircleArrowRightFilled />}>Enter</Button> */}
+            <Button variant="default" radius={theme.radius.lg - 2}>
+              Log In
+            </Button>
+            <Button radius={theme.radius.lg - 2}>Sign Up</Button>
           </Group>
 
           <Burger
@@ -486,11 +399,7 @@ export const HeaderWithMegaMenu: FunctionComponent = () => {
               <IconChevronDown size={16} color={theme.colors.gray[5]} stroke={3} />
             </Center>
           </UnstyledButton>
-          <Collapse in={linksOpened}>
-            {links.products}
-            <Divider my="sm" color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"} />
-            {links.others}
-          </Collapse>
+          <Collapse in={linksOpened}>{MenuButtonsWithIcon(links.product)}</Collapse>
 
           <a href="#" className={classes.link}>
             Business
