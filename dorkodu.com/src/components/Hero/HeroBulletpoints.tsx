3c9baaac432a341875b8b03eed95673cfd064175
doruk eray <doruk@dorkodu.com>
2023-03-06 23:38:23 +0300

    Update HeroBulletpoints.tsx

--- dorkodu.com/src/components/Hero/HeroBulletpoints.tsx
+++ dorkodu.com/src/components/Hero/HeroBulletpoints.tsx
@@ -1,72 +1,68 @@
-import { createStyles, Image, Container, Title, Button, Group, Text, List, ThemeIcon } from "@mantine/core";
+import { createStyles, Image, Container, Title, Button, Group, Text, List, ThemeIcon, rem } from "@mantine/core";
 import { IconCheck } from "@tabler/icons-react";
+import image from "./image.svg";
 
-const styles = {
-  HeroWithBulletpoints: createStyles((theme) => ({
-    inner: {
-      display: "flex",
-      justifyContent: "space-between",
-      paddingBottom: theme.spacing.xl * 4,
+const useStyles = createStyles((theme) => ({
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    paddingTop: `calc(${theme.spacing.xl} * 4)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 4)`,
+  },
+
+  content: {
+    maxWidth: rem(480),
+    marginRight: `calc(${theme.spacing.xl} * 3)`,
+
+    [theme.fn.smallerThan("md")]: {
+      maxWidth: "100%",
+      marginRight: 0,
     },
+  },
 
-    content: {
-      maxWidth: 480,
-      marginRight: theme.spacing.xl * 3,
+  title: {
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: rem(44),
+    lineHeight: 1.2,
+    fontWeight: 900,
 
-      [theme.fn.smallerThan("md")]: {
-        maxWidth: "100%",
-        marginRight: 0,
-      },
+    [theme.fn.smallerThan("xs")]: {
+      fontSize: rem(28),
     },
+  },
 
-    title: {
-      color: theme.colorScheme === "dark" ? theme.white : theme.black,
-      fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-      fontSize: 44,
-      lineHeight: 1.2,
-      fontWeight: 900,
-
-      [theme.fn.smallerThan("xs")]: {
-        fontSize: 28,
-      },
-    },
-
-    control: {
-      [theme.fn.smallerThan("xs")]: {
-        flex: 1,
-      },
-    },
-
-    image: {
+  control: {
+    [theme.fn.smallerThan("xs")]: {
       flex: 1,
-
-      [theme.fn.smallerThan("md")]: {
-        display: "none",
-      },
     },
+  },
 
-    highlight: {
-      position: "relative",
-      backgroundColor: theme.fn.variant({
-        variant: "light",
-        color: theme.primaryColor,
-      }).background,
-      borderRadius: theme.radius.sm,
-      padding: "4px 12px",
+  image: {
+    flex: 1,
+
+    [theme.fn.smallerThan("md")]: {
+      display: "none",
     },
-  })),
-};
+  },
 
-export function HeroWithBulletpoints() {
-  const { classes } = styles.HeroWithBulletpoints();
+  highlight: {
+    position: "relative",
+    backgroundColor: theme.fn.variant({ variant: "light", color: theme.primaryColor }).background,
+    borderRadius: theme.radius.sm,
+    padding: `${rem(4)} ${rem(12)}`,
+  },
+}));
 
+export function HeroBullets() {
+  const { classes } = useStyles();
   return (
     <div>
       <Container>
         <div className={classes.inner}>
           <div className={classes.content}>
             <Title className={classes.title}>
-              A <Text className={classes.highlight}>modern</Text> React <br /> components library
+              A <span className={classes.highlight}>modern</span> React <br /> components library
             </Title>
             <Text color="dimmed" mt="md">
               Build fully functional accessible web applications faster than ever – Mantine includes more than 120
@@ -79,7 +75,7 @@ export function HeroWithBulletpoints() {
               size="sm"
               icon={
                 <ThemeIcon size={20} radius="xl">
-                  <IconCheck size={12} stroke={1.5} />
+                  <IconCheck size={rem(12)} stroke={1.5} />
                 </ThemeIcon>
               }
             >
@@ -95,15 +91,15 @@ export function HeroWithBulletpoints() {
             </List>
 
             <Group mt={30}>
-              <Button radius="lg" size="md" className={classes.control}>
+              <Button radius="xl" size="md" className={classes.control}>
                 Get started
               </Button>
-              <Button variant="default" radius="lg" size="md" className={classes.control}>
+              <Button variant="default" radius="xl" size="md" className={classes.control}>
                 Source code
               </Button>
             </Group>
           </div>
-          <Image src="/images/dorkodu-work-culture.svg" className={classes.image} />
+          <Image src={image.src} className={classes.image} />
         </div>
       </Container>
     </div>
