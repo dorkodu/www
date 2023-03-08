52f191015b7d757808cc5c0a0eb7316b8339fb3a
doruk eray <doruk@dorkodu.com>
2023-03-08 23:32:59 +0300

    Update Product.tsx

--- dorkodu.com/src/components/Product.tsx
+++ dorkodu.com/src/components/Product.tsx
@@ -1,20 +1,22 @@
-import { Group, Image, SimpleGrid, Stack, Text } from "@mantine/core";
+import { Group, Image, Paper, SimpleGrid, Stack, Text } from "@mantine/core";
 
-export type ProductCardData = { title: string; tagline: string; icon: string };
+export type ProductCardData = { title: string; tagline: string; icon: string; link: string };
 
 export const ProductCard = ({ product }: { product: ProductCardData }) => {
   return (
-    <Group spacing="sm">
-      <Image src={product.icon} width={60} height={60} fit="contain" alt={product.title} withPlaceholder />
-      <Stack spacing={0}>
-        <Text weight={800} size={24}>
-          {product.title}
-        </Text>
-        <Text weight={500} color="dimmed" size="md">
-          {product.tagline}
-        </Text>
-      </Stack>
-    </Group>
+    <Paper p={10} component="a" href={product.link} withBorder shadow="sm" radius="lg">
+      <Group spacing="sm">
+        <Image src={product.icon} width={60} height={60} fit="contain" alt={product.title} withPlaceholder />
+        <Stack spacing={0}>
+          <Text weight={800} size={24}>
+            {product.title}
+          </Text>
+          <Text weight={500} color="dimmed" size="md">
+            {product.tagline}
+          </Text>
+        </Stack>
+      </Group>
+    </Paper>
   );
 };
 
