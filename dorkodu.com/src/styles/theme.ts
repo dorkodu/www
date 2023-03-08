e00df6be920d571bada198b07e2fd59c3aa333f2
doruk eray <doruk@dorkodu.com>
2023-03-08 23:33:08 +0300

    Update theme.ts

--- dorkodu.com/src/styles/theme.ts
+++ dorkodu.com/src/styles/theme.ts
@@ -5,13 +5,9 @@ const theme: MantineThemeOverride = {
   ...PrismTheme,
 
   focusRing: "auto",
-
   respectReducedMotion: true,
-
   cursorType: "pointer",
-
   defaultRadius: "md",
-
   dir: "ltr",
 
   white: tokens.color.gray(100),
@@ -34,7 +30,7 @@ const theme: MantineThemeOverride = {
 
   primaryColor: "green",
 
-  defaultGradient: { deg: 60, from: "dorkodu", to: "lime" },
+  defaultGradient: { deg: 60, from: "green", to: "lime" },
 
   fontFamily: `Rubik, Roboto, -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, sans-serif`,
 
@@ -47,73 +43,16 @@ const theme: MantineThemeOverride = {
     fontFamily: "Rubik",
 
     sizes: {
-      h1: { fontSize: 34, lineHeight: 1.25, fontWeight: 800 },
-      h2: { fontSize: 28, lineHeight: 1.25, fontWeight: 750 },
-      h3: { fontSize: 24, lineHeight: 1.3, fontWeight: 650 },
-      h4: { fontSize: 20, lineHeight: 1.3, fontWeight: 600 },
-      h5: { fontSize: 18, lineHeight: 1.35, fontWeight: 550 },
-      h6: { fontSize: 16, lineHeight: 1.35, fontWeight: 500 },
+      h1: { lineHeight: 1.25, fontWeight: 800 },
+      h2: { lineHeight: 1.25, fontWeight: 750 },
+      h3: { lineHeight: 1.3, fontWeight: 650 },
+      h4: { lineHeight: 1.3, fontWeight: 600 },
+      h5: { lineHeight: 1.35, fontWeight: 550 },
+      h6: { lineHeight: 1.35, fontWeight: 500 },
     },
   },
 
-  fontSizes: {
-    xs: 12,
-    sm: 14,
-    md: 16,
-    lg: 18,
-    xl: 20,
-  },
-
-  radius: {
-    xs: 2,
-    sm: 4,
-    md: 8,
-    lg: 16,
-    xl: 32,
-  },
-
-  spacing: {
-    xs: 10,
-    sm: 12,
-    md: 16,
-    lg: 20,
-    xl: 24,
-  },
-
-  breakpoints: {
-    xs: 500,
-    sm: 768,
-    md: 900,
-    lg: 1200,
-    xl: 1400,
-  },
-
-  shadows: {
-    xs: "0 1px 3px rgba(0, 0, 0, 0.05), 0 1px 2px rgba(0, 0, 0, 0.1)",
-    sm: "0 1px 3px rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05) 0px 10px 15px -5px, rgba(0, 0, 0, 0.04) 0px 7px 7px -5px",
-    md: "0 1px 3px rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px",
-    lg: "0 1px 3px rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05) 0px 28px 23px -7px, rgba(0, 0, 0, 0.04) 0px 12px 12px -7px",
-    xl: "0 1px 3px rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05) 0px 36px 28px -7px, rgba(0, 0, 0, 0.04) 0px 17px 17px -7px",
-  },
-
-  loader: "dots",
-
   globalStyles: (theme: MantineTheme) => ({}),
-
-  focusRingStyles: {
-    styles: (theme: MantineTheme) => ({
-      outlineOffset: 2,
-      outline: `2px solid ${theme.colors["blue"][theme.colorScheme === "dark" ? 7 : 5]}`,
-    }),
-    resetStyles: () => ({ outline: "none" }),
-    inputStyles: (theme: MantineTheme) => ({
-      outline: "none",
-      borderColor:
-        theme.colors["blue"][
-          typeof theme.primaryShade === "object" ? theme.primaryShade[theme.colorScheme] : theme.primaryShade
-        ],
-    }),
-  },
 };
 
 export default theme;
