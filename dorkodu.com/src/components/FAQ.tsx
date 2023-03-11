84e93ef5be795175a728083285f65d603642ccc1
doruk eray <doruk@dorkodu.com>
2023-03-11 23:17:11 +0300

    update: FAQ

--- dorkodu.com/src/components/FAQ.tsx
+++ dorkodu.com/src/components/FAQ.tsx
@@ -1,57 +1,67 @@
-import { Container, Title, Accordion, createStyles } from "@mantine/core";
-
-const useStyles = createStyles((theme) => ({
-  wrapper: {
-    paddingTop: theme.spacing.xl * 2,
-    paddingBottom: theme.spacing.xl * 2,
-    minHeight: 650,
-  },
-
-  title: {
-    marginBottom: theme.spacing.xl * 1.5,
-  },
-
-  item: {
-    borderRadius: theme.radius.md,
-    marginBottom: theme.spacing.lg,
-
-    border: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]}`,
-  },
-}));
+import { useTheme } from "@emotion/react";
+import { Container, Title, Accordion, createStyles, rem, ThemeIcon, useMantineTheme } from "@mantine/core";
+import { IconChevronDown } from "@tabler/icons-react";
 
 const placeholder =
   "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.";
 
-export function FaqSimple() {
-  const { classes } = useStyles();
+export function FAQs() {
+  const theme = useMantineTheme();
+
   return (
-    <Container size="sm" className={classes.wrapper}>
-      <Title align="center" className={classes.title}>
-        Frequently Asked Questions
+    <Container
+      size="sm"
+      sx={{
+        paddingTop: `calc(${theme.spacing.md} * 2)`,
+        paddingBottom: `calc(${theme.spacing.md} * 2)`,
+        minHeight: 650,
+      }}
+    >
+      <Title align="center" sx={{ marginBottom: 20 }} order={2}>
+        FAQs
       </Title>
 
-      <Accordion variant="separated">
-        <Accordion.Item className={classes.item} value="reset-password">
+      <Accordion
+        variant="separated"
+        chevronSize={32}
+        chevron={<IconChevronDown size={24} color={theme.colors.gray[6]} />}
+        multiple
+        styles={{
+          item: {
+            borderRadius: theme.radius.md,
+            marginBottom: theme.spacing.lg,
+            border: `${rem(1)} solid ${theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[3]}`,
+            boxShadow: `2px 2px 8px ${theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[2]}`,
+          },
+          itemTitle: {},
+          label: {
+            fontWeight: 500,
+            paddingLeft: 16,
+            borderRadius: 8,
+          },
+        }}
+      >
+        <Accordion.Item value="reset-password">
           <Accordion.Control>How can I reset my password?</Accordion.Control>
           <Accordion.Panel>{placeholder}</Accordion.Panel>
         </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="another-account">
+        <Accordion.Item value="another-account">
           <Accordion.Control>Can I create more that one account?</Accordion.Control>
           <Accordion.Panel>{placeholder}</Accordion.Panel>
         </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="newsletter">
+        <Accordion.Item value="newsletter">
           <Accordion.Control>How can I subscribe to monthly newsletter?</Accordion.Control>
           <Accordion.Panel>{placeholder}</Accordion.Panel>
         </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="credit-card">
+        <Accordion.Item value="credit-card">
           <Accordion.Control>Do you store credit card information securely?</Accordion.Control>
           <Accordion.Panel>{placeholder}</Accordion.Panel>
         </Accordion.Item>
 
-        <Accordion.Item className={classes.item} value="payment">
+        <Accordion.Item value="payment">
           <Accordion.Control>What payment systems to you work with?</Accordion.Control>
           <Accordion.Panel>{placeholder}</Accordion.Panel>
         </Accordion.Item>
