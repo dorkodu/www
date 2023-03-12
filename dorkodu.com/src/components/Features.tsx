78c5fd0a8611e88d3e0ff856dab9076cf85e52f4
doruk eray <doruk@dorkodu.com>
2023-03-12 23:47:25 +0300

    new components: Features

---
+++ dorkodu.com/src/components/Features.tsx
@@ -0,0 +1,18 @@
+import {
+  Image,
+  Text,
+  Container,
+  ThemeIcon,
+  Title,
+  SimpleGrid,
+  createStyles,
+  rem,
+  Box,
+  Card,
+  Paper,
+} from "@mantine/core";
+
+const styles = {
+};
+
+export function UseCaseCard({ icon, title, description, image }: UseCase) {}
