75f8f0d155ef519aa4db50af2a7110b66196f88f
doruk eray <doruk@dorkodu.com>
2023-03-15 23:12:57 +0300

    Update Product.tsx

--- dorkodu.com/src/components/Product.tsx
+++ dorkodu.com/src/components/Product.tsx
@@ -1,11 +1,11 @@
-import { Group, Image, Paper, SimpleGrid, Stack, Text } from "@mantine/core";
+import { Grid, Group, Image, Paper, SimpleGrid, Stack, Text } from "@mantine/core";
 
 export type ProductCardData = { title: string; tagline: string; icon: string; link: string };
 
 export const ProductLinkCard = ({ product }: { product: ProductCardData }) => {
   return (
     <Paper p={10} component="a" href={product.link} withBorder shadow="sm" radius="lg">
-      <Group spacing="sm">
+      <Group spacing="sm" pr={12}>
         <Image src={product.icon} width={60} height={60} fit="contain" alt={product.title} withPlaceholder />
         <Stack spacing={0}>
           <Text weight={800} size={24}>
@@ -21,9 +21,11 @@ export const ProductLinkCard = ({ product }: { product: ProductCardData }) => {
 };
 
 export const ProductShowcaseGrid = ({ products }: { products: ProductCardData[] }) => (
-  <SimpleGrid my={20} cols={3} spacing={16}>
+  <Grid my={20} gutter={8}>
     {products.map((item) => (
-      <ProductLinkCard product={item} />
+      <Grid.Col span={"content"}>
+        <ProductLinkCard product={item} />
+      </Grid.Col>
     ))}
-  </SimpleGrid>
+  </Grid>
 );
