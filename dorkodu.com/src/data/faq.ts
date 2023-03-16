6dbfc2cfa034dfac5cece52e1af61905b1e9d3b8
doruk eray <doruk@dorkodu.com>
2023-03-16 19:16:12 +0300

    add new data files

---
+++ dorkodu.com/src/data/faq.ts
@@ -0,0 +1,13 @@
+export const faqs: [string, string][] = [
+  ["What is Dorkodu?", ""],
+  ["How much it costs?", "It is free to create a Dorkodu account, and will always be."],
+  ["What does Dorkodu company do?", "We are some kind of a personal growth companion for your life."],
+  [
+    `What does "dream fulfillment technology company" mean?`,
+    "This is a simple statement which explains our purpose. We are here to make all your dreams possible, help you make them come true, with the help of meaningful, humane technology and our apps, which are tools for your mind.",
+  ],
+  [
+    `Is there any mobile apps?`,
+    "To install a Dorkodu app, visit its website and will suggest you to install it. Add it to your home screen and enjoy using Dorkodu!",
+  ],
+];
