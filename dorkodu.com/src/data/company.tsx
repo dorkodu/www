ea22df7f3030c4debbc388a6163a6a62b7d34412
doruk eray <doruk@dorkodu.com>
2023-03-21 01:37:30 +0300

    new data source: company

---
+++ dorkodu.com/src/data/company.tsx
@@ -0,0 +1,36 @@
+export const people = {
+  theRing: [
+    {
+      name: "Doruk Eray",
+      title: "Founder & Chief",
+      about: "Cult Leader, Product Polymath, Lunatic Craftsman, Revolutionist.",
+      tags: ["Product", "Software", "UI/UX", "Art", "Business"],
+      avatar: "/images/doruk.png",
+      icons: <></>,
+    },
+    {
+      name: "Berk Cambaz",
+      title: "co-founding technologist",
+      about: "Swiss-knife, Homebrew Software, Research & Development",
+      tags: ["Technology", "Software", "Infrastructure", "Open-source", "Dev-Ops", "Economics"],
+      avatar: "/images/berk.png",
+      icons: <></>,
+    },
+  ],
+  members: [
+    {
+      name: "Furkan Y.",
+      title: "Rookie",
+      about: "Hustler, Operations, Social Media",
+      tags: ["Media", "Business", "Operations", "Marketing"],
+      icons: <></>,
+    },
+    {
+      name: "Aziz Xcmaz",
+      title: "Rookie",
+      about: "Graphics Design, Games, Product. Gnome Chieftain.",
+      tags: ["Art", "Games", "Design"],
+      icons: <></>,
+    },
+  ],
+};
