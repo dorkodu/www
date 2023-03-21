24bf470bb51383f104cdab56282be2a485e5e4c8
doruk eray <doruk@dorkodu.com>
2023-03-21 23:25:38 +0300

    Update Member.tsx

--- dorkodu.com/src/components/Member.tsx
+++ dorkodu.com/src/components/Member.tsx
@@ -1,66 +1,75 @@
 import Dorkodu from "../types/dorkodu";
 
-import { createStyles, Avatar, Text, Group, Stack } from "@mantine/core";
+import { createStyles, Avatar, Text, Group, Stack, Badge, Box } from "@mantine/core";
 import { IconPhoneCall, IconAt } from "@tabler/icons-react";
+import { useMediaQuery } from "@mantine/hooks";
 
 const useStyles = createStyles((theme) => ({
   icon: {
     color: theme.colorScheme === "dark" ? theme.colors.dark[3] : theme.colors.gray[5],
   },
-
   name: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    color: theme.colorScheme == "dark" ? theme.white : theme.colors.dark[9],
   },
 }));
 
-export function TeamMember({ member }: { member: Dorkodu.TeamMember }) {
-  const { classes } = useStyles();
+export function TheRingMember({ member }: { member: Dorkodu.TeamMember }) {
+  const { classes, theme } = useStyles();
+
+  const smolScreen = useMediaQuery("(min-width: 500px)");
 
   return (
-    <Stack>
-      <Avatar src={member.avatar} size={94} radius="md" />
-
-      <Text fz="xs" tt="uppercase" fw={700} c="dimmed">
-        {member.title}
-      </Text>
-
-      <Text fz="lg" fw={500} className={classes.name}>
-        {member.name}
-      </Text>
-
-      <Group spacing={10} mt={3}>
-        {member.icons}
+    <Box maw={500}>
+      <Group noWrap={smolScreen ? true : false} align="flex-start">
+        <Avatar src={member.avatar} size={100} radius="lg" />
+        <Stack spacing={0}>
+          <Text fz="xs" tt="uppercase" fw={700} c="dimmed">
+            {member.title}
+          </Text>
+          <Text fz="xl" fw={700} className={classes.name}>
+            {member.name}
+          </Text>
+          <Text fz="sm" weight={500} m={0} p={0} c="dimmed" my={5}>
+            {member.about}
+          </Text>
+          <Group spacing={5} mt={4}>
+            {member.tags?.map((tag) => (
+              <Badge radius={8} px={8} variant="gradient">
+                {tag}
+              </Badge>
+            ))}
+          </Group>
+        </Stack>
       </Group>
-    </Stack>
+    </Box>
   );
 }
 
-export function TheRingMember({ member }: { member: Dorkodu.TeamMember }) {
-  const { classes } = useStyles();
+export function TeamMember({ member }: { member: Dorkodu.TeamMember }) {
+  const { classes, theme } = useStyles();
 
   return (
-    <div>
+    <Box maw={500}>
       <Group noWrap>
-        <Avatar src={member.avatar} size={94} radius="md" />
+        <Avatar src={member.avatar} size={76} radius="lg" />
         <div>
           <Text fz="xs" tt="uppercase" fw={700} c="dimmed">
             {member.title}
           </Text>
 
-          <Text fz="lg" fw={500} className={classes.name}>
+          <Text fz="xl" fw={650}>
             {member.name}
           </Text>
 
-          <Group noWrap spacing={10} mt={3}>
-            {member.icons}
-
-            <IconAt stroke={1.5} size="1rem" className={classes.icon} />
+          <Group spacing={5} mt={3}>
+            {member.tags?.map((tag) => (
+              <Badge radius={8} px={8} variant="light">
+                {tag}
+              </Badge>
+            ))}
           </Group>
-          <Text fz="sm" c="dimmed" my={5}>
-            {member.about}
-          </Text>
         </div>
       </Group>
-    </div>
+    </Box>
   );
 }
