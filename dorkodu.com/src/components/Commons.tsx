31b2a3e66784cff6d808e204f74cc1984f7c609d
doruk eray <doruk@dorkodu.com>
2023-03-21 23:25:16 +0300

    new component: StarDivider

--- dorkodu.com/src/components/Commons.tsx
+++ dorkodu.com/src/components/Commons.tsx
@@ -1,3 +1,19 @@
+export const StarDivider = () => {
+  const theme = useMantineTheme();
+
+  return (
+    <Text
+      weight={600}
+      size="5rem"
+      my={"2.75vw"}
+      mr={5}
+      color={theme.colorScheme == "dark" ? theme.colors.dark[6] : theme.colors.gray[3]}
+      align="center"
+    >
+      *****
+    </Text>
+  );
+};
 
 export const StoryCard = ({ icon, text, order }: { icon: React.ReactNode; text: React.ReactNode; order: number }) => {
   const theme = useMantineTheme();
