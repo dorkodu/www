9c8d6c1b17d33739636fad7c3087a2597c92fb3c
doruk eray <doruk@dorkodu.com>
2023-03-22 23:58:01 +0300

    Update fonts.ts

--- dorkodu.com/src/styles/fonts.ts
+++ dorkodu.com/src/styles/fonts.ts
@@ -1,4 +1,4 @@
 import { Rubik as RubikFont } from "@next/font/google";
 
 //? Fonts
-export const Rubik = RubikFont({ subsets: ["latin", "latin-ext"], style: ["normal"] });
+export const Rubik = RubikFont({ weight: "variable", subsets: ["latin", "latin-ext"], style: ["normal", "italic"] });
