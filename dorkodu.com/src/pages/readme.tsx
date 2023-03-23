fb472429e160f1195a42b62a9cc87d6d6458f71a
doruk eray <doruk@dorkodu.com>
2023-03-23 23:43:20 +0300

    Update readme.tsx

--- dorkodu.com/src/pages/readme.tsx
+++ dorkodu.com/src/pages/readme.tsx
@@ -1,13 +1,14 @@
 import Meta from "@/components/Meta";
 import WebsiteLayout from "@/layouts/WebsiteLayout";
-import { createStyles, Title, Text, Button, Container, Group, rem } from "@mantine/core";
+import { createStyles, Title, Text, Button, Container, Group, rem, Stack, Image, List } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { IconArrowRight } from "@tabler/icons-react";
 import { NextPage } from "next";
 import Head from "next/head";
 
 const meta = {
   title: "README",
-  description: `A Story of.`,
+  description: `A Story of...`,
   keywords: `dorkodu, dorkodu.com, dorkodia, liberate, gamification, productivity, humane technology`,
   url: "/",
   image: {
@@ -22,11 +23,144 @@ const Readme: NextPage = () => {
       <Head>
         <Meta {...meta} />
       </Head>
-      <main>
-        <Title></Title>
-      </main>
+      <Hero />
+      <Jobs />
     </WebsiteLayout>
   );
 };
 
 export default Readme;
+
+const styles = {
+  Hero: createStyles((theme) => ({
+    title: {
+      color: theme.colorScheme == "dark" ? theme.colors.gray[6] : theme.colors.gray[5],
+      fontSize: rem(28),
+      lineHeight: 1.2,
+      fontWeight: 700,
+      letterSpacing: -0.75,
+
+      [theme.fn.smallerThan("xs")]: {
+        fontSize: rem(28),
+      },
+    },
+
+    headline: {
+      color: theme.colorScheme == "dark" ? theme.white : theme.black,
+      letterSpacing: -0.8,
+    },
+
+    description: {},
+
+    inner: {
+      display: "flex",
+      justifyContent: "space-between",
+      paddingTop: `calc(${theme.spacing.xl} * 4)`,
+      paddingBottom: `calc(${theme.spacing.xl} * 4)`,
+
+      [theme.fn.smallerThan("md")]: {
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 40,
+      },
+    },
+
+    content: {
+      maxWidth: rem(600),
+      marginRight: `calc(${theme.spacing.xl} * 3)`,
+
+      [theme.fn.smallerThan("md")]: {
+        maxWidth: "100%",
+        marginRight: 0,
+      },
+    },
+
+    image: {
+      maxWidth: 400,
+    },
+  })),
+};
+
+const Hero = () => {
+  const { classes: $, theme } = styles.Hero();
+
+  return (
+    <Container>
+      <div className={$.inner}>
+        <div className={$.content}>
+          <Title order={1} size="h2" color="dimmed" className={$.title}>
+            About Dorkodu
+          </Title>
+          <Title order={2} size="h1" weight={800} className={$.headline}>
+            <Text span>Your</Text>
+            <Text variant="gradient" gradient={{ from: "blue", to: "cyan", deg: 45 }} span>
+              &nbsp;Dream Fulfillment&nbsp;
+            </Text>
+            <Text mt={-8}>Technology Company.</Text>
+          </Title>
+          <Text my={8} size={18} maw={425}>
+            We combine gamification, human-centric design with our superior technology to create products that help
+            people&nbsp;
+            <Text span weight={500}>
+              fulfill their dreams
+            </Text>
+            .
+          </Text>
+
+          <List
+            mt={20}
+            spacing={8}
+            icon={
+              <Text size={28} color="green">
+                ⁕
+              </Text>
+            }
+          >
+            <List.Item>
+              <Text weight={600}>Free & Open Source.</Text>
+              <Text color="dimmed">All packages have MIT license.</Text>
+            </List.Item>
+            <List.Item>
+              <Text weight={600}>Free & Open Source.</Text>
+              <Text color="dimmed">All packages have MIT license.</Text>
+            </List.Item>
+            <List.Item>
+              <Text weight={600}>Free & Open Source.</Text>
+              <Text color="dimmed">All packages have MIT license.</Text>
+            </List.Item>
+          </List>
+        </div>
+        <Image src="/images/undraw/co-workers.svg" className={$.image} />
+      </div>
+    </Container>
+  );
+};
+
+const Jobs = () => {
+  const isBigScreen = useMediaQuery("(min-width: 760px)");
+
+  return (
+    <Container my={50}>
+      <Group noWrap={isBigScreen} spacing={20}>
+        <Stack spacing={0} maw={600}>
+          <Title order={2}>Work @ Dorkodu</Title>
+          <Text color="dimmed" weight={500} size="lg">
+            Join the revolution.
+          </Text>
+          <Text my={8}>
+            {`
+          We're proud of our mission and the business we've built around it. 
+          We work really hard, and most days we love our jobs. 
+        `}
+          </Text>
+          <div>
+            <Button variant="gradient" rightIcon={<IconArrowRight />} size="lg" radius={18} mt={10}>
+              Explore Open Roles
+            </Button>
+          </div>
+        </Stack>
+        <Image src="/images/undraw_pair-programming.svg" alt="" />
+      </Group>
+    </Container>
+  );
+};
