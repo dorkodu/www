fde23062d6bffbdb7c6752f06ba03b03b2847385
doruk eray <doruk@dorkodu.com>
2023-03-23 23:43:15 +0300

    Update about.tsx

--- dorkodu.com/src/pages/about.tsx
+++ dorkodu.com/src/pages/about.tsx
@@ -13,7 +13,11 @@ import {
   Group,
   Box,
   createStyles,
+  Highlight,
 } from "@mantine/core";
+import { Button, rem } from "@mantine/core";
+import { IconArrowRight } from "@tabler/icons-react";
+import { useMediaQuery } from "@mantine/hooks";
 
 import WebsiteLayout from "@/layouts/WebsiteLayout";
 import Meta from "@/components/Meta";
@@ -41,17 +45,124 @@ const About: NextPage = () => {
       <Head>
         <Meta {...meta} />
       </Head>
+
       <Hero />
       <OurTeam />
       <StarDivider />
       <Story />
       <Jobs />
+      <Investors />
       <Press />
     </WebsiteLayout>
   );
 };
 
 export default About;
+
+const styles = {
+  Hero: createStyles((theme) => ({
+    title: {
+      color: theme.colorScheme == "dark" ? theme.colors.gray[6] : theme.colors.gray[5],
+      fontSize: rem(28),
+      lineHeight: 1.2,
+      fontWeight: 700,
+      letterSpacing: -0.75,
+
+      [theme.fn.smallerThan("xs")]: {
+        fontSize: rem(28),
+      },
+    },
+
+    headline: {
+      color: theme.colorScheme == "dark" ? theme.white : theme.black,
+      letterSpacing: -0.8,
+    },
+
+    description: {},
+
+    inner: {
+      display: "flex",
+      justifyContent: "space-between",
+      paddingTop: `calc(${theme.spacing.xl} * 4)`,
+      paddingBottom: `calc(${theme.spacing.xl} * 4)`,
+
+      [theme.fn.smallerThan("md")]: {
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 40,
+      },
+    },
+
+    content: {
+      maxWidth: rem(600),
+      marginRight: `calc(${theme.spacing.xl} * 3)`,
+
+      [theme.fn.smallerThan("md")]: {
+        maxWidth: "100%",
+        marginRight: 0,
+      },
+    },
+
+    image: {
+      maxWidth: 400,
+    },
+  })),
+};
+
+const Hero = () => {
+  const { classes: $, theme } = styles.Hero();
+
+  return (
+    <Container>
+      <div className={$.inner}>
+        <div className={$.content}>
+          <Title order={1} size="h2" color="dimmed" className={$.title}>
+            About Dorkodu
+          </Title>
+          <Title order={2} size="h1" weight={800} className={$.headline}>
+            <Text span>Your</Text>
+            <Text variant="gradient" gradient={{ from: "blue", to: "cyan", deg: 45 }} span>
+              &nbsp;Dream Fulfillment&nbsp;
+            </Text>
+            <Text mt={-8}>Technology Company.</Text>
+          </Title>
+          <Text my={8} size={18} maw={425}>
+            We combine gamification, human-centric design with our superior technology to create products that help
+            people&nbsp;
+            <Text span weight={500}>
+              fulfill their dreams
+            </Text>
+            .
+          </Text>
+
+          <List
+            mt={20}
+            spacing={8}
+            icon={
+              <Text size={28} color="green">
+                ⁕
+              </Text>
+            }
+          >
+            <List.Item>
+              <Text weight={600}>Free & Open Source.</Text>
+              <Text color="dimmed">All packages have MIT license.</Text>
+            </List.Item>
+            <List.Item>
+              <Text weight={600}>Free & Open Source.</Text>
+              <Text color="dimmed">All packages have MIT license.</Text>
+            </List.Item>
+            <List.Item>
+              <Text weight={600}>Free & Open Source.</Text>
+              <Text color="dimmed">All packages have MIT license.</Text>
+            </List.Item>
+          </List>
+        </div>
+        <Image src="/images/undraw/co-workers.svg" className={$.image} />
+      </div>
+    </Container>
+  );
+};
 
 const Jobs = () => {
   const isBigScreen = useMediaQuery("(min-width: 760px)");
@@ -82,49 +193,64 @@ const Jobs = () => {
   );
 };
 
-const Press = () => {
+const Investors = () => {
+  const isBigScreen = useMediaQuery("(min-width: 760px)");
+
   return (
-    <Container size={600}>
-      <Title order={2}>Press</Title>
-      <Text color="dimmed">
-        {`
-          We're proud of our mission and the business we've built around it. 
-          We work really hard, and most days we love our jobs. 
-          Learn about working at Dorkodu and our current openings.
-        `}
-      </Text>
-      <Button variant="gradient" rightIcon={<IconArrowRight />}>
-        Explore Open Roles
-      </Button>
-      <Image src="" alt="" />
+    <Container my={50}>
+      <Group noWrap={isBigScreen} spacing={20}>
+        <Stack spacing={0} maw={600}>
+          <Title order={2}>Press</Title>
+          <Text color="dimmed" weight={500} size="lg">
+            Look, we hit the news{" "}
+            <b>
+              <i>!</i>
+            </b>
+          </Text>
+          <Text my={8}>
+            {`
+              We're proud of our mission and the business we've built around it. 
+              We work really hard, and most days we love our jobs. 
+          `}
+          </Text>
+          <div>
+            <Button variant="gradient" rightIcon={<IconArrowRight />} size="lg" radius={18} mt={10}>
+              Explore Open Roles
+            </Button>
+          </div>
+        </Stack>
+        <Image src="/images/undraw/.svg" alt="" />
+      </Group>
     </Container>
   );
 };
 
-const styles = {
-  Hero: createStyles((theme) => ({
-    title: {
-      color: theme.colorScheme == "dark" ? theme.colors.gray[6] : theme.colors.gray[5],
-    },
-    tagline: {
-      color: theme.colorScheme == "dark" ? theme.white : theme.black,
-    },
-    description: {},
-  })),
-};
-
-const Hero = () => {
-  const { classes: x, theme } = styles.Hero();
+const Press = () => {
+  const isBigScreen = useMediaQuery("(min-width: 760px)");
 
   return (
-    <>
-      <Title order={1} size="h2" color="dimmed" className={x.title}>
-        About Dorkodu
-      </Title>
-      <Title order={2} size="h1" weight={800} className={x.tagline}>
-        We are your <Text<"span"> variant="gradient">dream fulfillment</Text> <Text></Text> company.
-      </Title>
-    </>
+    <Container my={50}>
+      <Group noWrap={isBigScreen} spacing={20}>
+        <Stack spacing={0} maw={600}>
+          <Title order={2}>Press</Title>
+          <Text color="dimmed" weight={500} size="lg">
+            We are on the news!
+          </Text>
+          <Text my={8}>
+            {`
+          We're proud of our mission and the business we've built around it. 
+          We work really hard, and most days we love our jobs. 
+        `}
+          </Text>
+          <div>
+            <Button variant="gradient" rightIcon={<IconArrowRight />} size="lg" radius={18} mt={10}>
+              Explore Open Roles
+            </Button>
+          </div>
+        </Stack>
+        <Image src="/images/undraw/news.svg" alt="News" maw={400} />
+      </Group>
+    </Container>
   );
 };
 
@@ -177,101 +303,3 @@ const something = (
     <Image />
   </div>
 );
-
-import { Button, ThemeIcon, rem } from "@mantine/core";
-import { IconArrowRight, IconCheck } from "@tabler/icons-react";
-import { useMediaQuery } from "@mantine/hooks";
-
-const useStyles = createStyles((theme) => ({
-  inner: {
-    display: "flex",
-    justifyContent: "space-between",
-    paddingTop: `calc(${theme.spacing.xl} * 4)`,
-    paddingBottom: `calc(${theme.spacing.xl} * 4)`,
-  },
-
-  content: {
-    maxWidth: rem(480),
-    marginRight: `calc(${theme.spacing.xl} * 3)`,
-
-    [theme.fn.smallerThan("md")]: {
-      maxWidth: "100%",
-      marginRight: 0,
-    },
-  },
-
-  title: {
-    color: theme.colorScheme === "dark" ? theme.white : theme.black,
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontSize: rem(44),
-    lineHeight: 1.2,
-    fontWeight: 900,
-
-    [theme.fn.smallerThan("xs")]: {
-      fontSize: rem(28),
-    },
-  },
-
-  control: {
-    [theme.fn.smallerThan("xs")]: {
-      flex: 1,
-    },
-  },
-
-  image: {
-    flex: 1,
-
-    [theme.fn.smallerThan("md")]: {
-      display: "none",
-    },
-  },
-
-  highlight: {
-    position: "relative",
-    backgroundColor: theme.fn.variant({ variant: "light", color: theme.primaryColor }).background,
-    borderRadius: theme.radius.sm,
-    padding: `${rem(4)} ${rem(12)}`,
-  },
-}));
-
-export function HeroBullets() {
-  const { classes: $ } = useStyles();
-
-  return (
-    <Container>
-      <div className={$.inner}>
-        <div className={$.content}>
-          <Title className={$.title}>
-            A <span className={$.highlight}>modern</span> React <br /> components library
-          </Title>
-          <Text color="dimmed" mt="md">
-            Build fully functional accessible web applications faster than ever – Mantine includes more than 120
-            customizable components and hooks to cover you in any situation
-          </Text>
-
-          <List mt={30}>
-            <List.Item>
-              <b>TypeScript based</b> – build type safe applications, all components and hooks export types
-            </List.Item>
-            <List.Item>
-              <b>Free and open source</b> – all packages have MIT license, you can use Mantine in any project
-            </List.Item>
-            <List.Item>
-              <b>No annoying focus ring</b> – focus ring will appear only when user navigates with keyboard
-            </List.Item>
-          </List>
-
-          <Group mt={30}>
-            <Button radius="xl" size="md" className={$.control}>
-              Get started
-            </Button>
-            <Button variant="default" radius="xl" size="md" className={$.control}>
-              Source code
-            </Button>
-          </Group>
-        </div>
-        <Image src={image.src} className={$.image} />
-      </div>
-    </Container>
-  );
-}
