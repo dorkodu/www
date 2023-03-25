36ac5c869d6d1090702196db94ccf9db98994494
doruk eray <doruk@dorkodu.com>
2023-03-25 23:39:06 +0300

    rename from manifesto to readme

--- dorkodu.com/src/pages/readme.tsx
+++ dorkodu.com/src/pages/readme.tsx
@@ -1,6 +1,6 @@
 import Meta from "@/components/Meta";
 import WebsiteLayout from "@/layouts/WebsiteLayout";
-import { createStyles, Title, Text, Button, Container, Group, rem, Stack, Image, List } from "@mantine/core";
+import { createStyles, Title, Text, Button, Container, Group, rem, Stack, Image, List, Box } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import { IconArrowRight } from "@tabler/icons-react";
 import { NextPage } from "next";
@@ -23,7 +23,15 @@ const Readme: NextPage = () => {
       <Head>
         <Meta {...meta} />
       </Head>
+
       <Hero />
+
+      <Container size={600} my={50}>
+        <ActI />
+        <ActI />
+        <ActI />
+      </Container>
+
       <Jobs />
     </WebsiteLayout>
   );
@@ -31,82 +39,35 @@ const Readme: NextPage = () => {
 
 export default Readme;
 
-const styles = {
-  Hero: createStyles((theme) => ({
-    title: {
-      color: theme.colorScheme == "dark" ? theme.colors.gray[6] : theme.colors.gray[5],
-      fontSize: rem(28),
-      lineHeight: 1.2,
-      fontWeight: 700,
-      letterSpacing: -0.75,
-
-      [theme.fn.smallerThan("xs")]: {
-        fontSize: rem(28),
-      },
-    },
-
-    headline: {
-      color: theme.colorScheme == "dark" ? theme.white : theme.black,
-      letterSpacing: -0.8,
-    },
-
-    description: {},
-
-    inner: {
-      display: "flex",
-      justifyContent: "space-between",
-      paddingTop: `calc(${theme.spacing.xl} * 4)`,
-      paddingBottom: `calc(${theme.spacing.xl} * 4)`,
-
-      [theme.fn.smallerThan("md")]: {
-        flexDirection: "column",
-        alignItems: "center",
-        gap: 40,
-      },
-    },
-
-    content: {
-      maxWidth: rem(600),
-      marginRight: `calc(${theme.spacing.xl} * 3)`,
-
-      [theme.fn.smallerThan("md")]: {
-        maxWidth: "100%",
-        marginRight: 0,
-      },
-    },
-
-    image: {
-      maxWidth: 400,
-    },
-  })),
-};
-
 const Hero = () => {
   const { classes: $, theme } = styles.Hero();
 
   return (
-    <Container>
+    <Container mt={25} mb={100}>
       <div className={$.inner}>
         <div className={$.content}>
-          <Title order={1} size="h2" color="dimmed" className={$.title}>
-            About Dorkodu
+          <Title
+            order={1}
+            size={48}
+            color="dimmed"
+            variant="gradient"
+            gradient={{ from: "#00cc44", to: "lime", deg: 180 }}
+            className={$.title}
+          >
+            README
           </Title>
           <Title order={2} size="h1" weight={800} className={$.headline}>
-            <Text span>Your</Text>
-            <Text variant="gradient" gradient={{ from: "blue", to: "cyan", deg: 45 }} span>
-              &nbsp;Dream Fulfillment&nbsp;
-            </Text>
-            <Text mt={-8}>Technology Company.</Text>
+            The Dorkodu Manifesto
           </Title>
           <Text my={8} size={18} maw={425}>
-            We combine gamification, human-centric design with our superior technology to create products that help
-            people&nbsp;
-            <Text span weight={500}>
-              fulfill their dreams
-            </Text>
-            .
+            <div>A story of tools for the human mind.</div>
           </Text>
-
+          <Text size={18} maw={425} color="dimmed">
+            <div>
+              Your very own set of tools to build and explore the new internet. From private notes to decentralized
+              communities.
+            </div>
+          </Text>
           <List
             mt={20}
             spacing={8}
@@ -163,4 +124,68 @@ const Jobs = () => {
       </Group>
     </Container>
   );
+};
+
+const ActI = () => {
+  return (
+    <StoryAct>
+      <Title>Act I.</Title>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, qui. Molestias expedita, alias sit maiores porro
+        nam veniam architecto itaque quo inventore laudantium obcaecati totam.
+      </Text>
+    </StoryAct>
+  );
+};
+
+const StoryAct = ({ children }: { children: React.ReactNode }) => {};
+
+const styles = {
+  Hero: createStyles((theme) => ({
+    title: {
+      color: theme.colorScheme == "dark" ? theme.colors.gray[6] : theme.colors.gray[5],
+      fontSize: rem(40),
+      lineHeight: 1,
+      fontWeight: 900,
+      letterSpacing: -1,
+
+      [theme.fn.smallerThan("xs")]: {
+        fontSize: rem(28),
+      },
+    },
+
+    headline: {
+      color: theme.colorScheme == "dark" ? theme.white : theme.black,
+      letterSpacing: -0.8,
+    },
+
+    description: {},
+
+    inner: {
+      display: "flex",
+      justifyContent: "space-between",
+      paddingTop: `calc(${theme.spacing.xl} * 4)`,
+      paddingBottom: `calc(${theme.spacing.xl} * 4)`,
+
+      [theme.fn.smallerThan("md")]: {
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 40,
+      },
+    },
+
+    content: {
+      maxWidth: rem(600),
+      marginRight: `calc(${theme.spacing.xl} * 3)`,
+
+      [theme.fn.smallerThan("md")]: {
+        maxWidth: "100%",
+        marginRight: 0,
+      },
+    },
+
+    image: {
+      maxWidth: 400,
+    },
+  })),
 };
