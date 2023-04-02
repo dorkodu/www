70a9d17ff39b8e0f6c2b686796c877d43eadf85c
doruk eray <doruk@dorkodu.com>
2023-04-02 23:49:36 +0300

    Update index.tsx

--- dorkodu.com/src/pages/products/index.tsx
+++ dorkodu.com/src/pages/products/index.tsx
@@ -1,36 +1,25 @@
+import Meta from "@/components/Meta";
+import WebsiteLayout from "@/layouts/WebsiteLayout";
+import { Emoji, PottedPlant } from "@/styles/twemoji";
+import { Badge, Container, Group, Paper, Text, Title } from "@mantine/core";
 import Head from "next/head";
 
-import { useState } from "react";
-import { AppShell, Navbar, Header, Footer, Aside, Text, MediaQuery, Burger, useMantineTheme } from "@mantine/core";
-
-import WebsiteLayout from "@/layouts/WebsiteLayout";
-import { NextPage } from "next";
-import Meta from "@/components/Meta";
-
-import { HeroIndex } from "@/components/Hero/HeroIndex";
-
 const meta = {
-  title: "welcome! — dorkodu",
-  description: `liberate the humankind.`,
-  keywords: `dorkodu, dorkodu.com, dorkodia, liberate, gamification, productivity, humane technology`,
-  url: "/",
-  image: {
-    src: "/public/images/dorkodu-single.png",
-    alt: "dorkodu logo with single letter.",
-  },
+  title: `Our Products — Dorkodu`,
+  description: ``,
+  keywords: `dorkodu products, dorkodu apps`,
+  url: "/products",
 };
 
-const Home: NextPage = () => {
+const Page = () => {
   return (
     <WebsiteLayout>
       <Head>
         <Meta {...meta} />
       </Head>
-      <main>
-        <HeroIndex />
-      </main>
+      <Hero />
     </WebsiteLayout>
   );
 };
 
-export default Home;
+export default Page;
