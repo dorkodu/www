1a30dc884edd97c0ca32beee0c869fbef7ef7470
doruk eray <doruk@dorkodu.com>
2023-08-08 17:02:33 +0300

    doruk: configs

--- doruk/vite.config.ts
+++ doruk/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@shared": path.resolve(__dirname, "../../shared/src"),
+      "@shared": path.resolve(__dirname, "../@shared/src"),
     },
   },
   build: {
