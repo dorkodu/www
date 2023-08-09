0d111cfec5170bd57f44002052cacad2bea99ecf
doruk eray <doruk@dorkodu.com>
2023-08-09 23:52:48 +0300

    Update theme.ts

--- doruk/src/styles/theme.ts
+++ doruk/src/styles/theme.ts
@@ -1,6 +1,24 @@
 import { MantineTheme, MantineThemeOverride } from "@mantine/core";
 
 export const theme: MantineThemeOverride = {
+  defaultGradient: { deg: 60, from: "green", to: "lime" },
+
+  lineHeight: 1.25,
+
+  headings: {
+    fontWeight: 600,
+    fontFamily: "Rubik",
+
+    sizes: {
+      h1: { lineHeight: 1.25, fontWeight: 800 },
+      h2: { lineHeight: 1.25, fontWeight: 750 },
+      h3: { lineHeight: 1.3, fontWeight: 650 },
+      h4: { lineHeight: 1.3, fontWeight: 600 },
+      h5: { lineHeight: 1.35, fontWeight: 550 },
+      h6: { lineHeight: 1.35, fontWeight: 500 },
+    },
+  },
+
   globalStyles: (theme: MantineTheme) => ({
     body: {
       overflowY: "scroll",
