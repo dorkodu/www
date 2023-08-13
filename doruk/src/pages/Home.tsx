74b0a12e2b0f8062a63968d4e2a41ab2fa89305a
doruk eray <doruk@dorkodu.com>
2023-08-13 14:17:41 +0300

    doruk: Home

--- doruk/src/pages/Home.tsx
+++ doruk/src/pages/Home.tsx
@@ -1,21 +1,35 @@
 import {
+  Box,
   Button,
   Container,
   Group,
   Image,
+  List,
   Stack,
   Text,
   Title,
+  useMantineTheme,
 } from "@mantine/core";
-import { Showcase } from "@shared/components/commons";
 
-import { IconArrowRight, IconExternalLink } from "@tabler/icons-react";
+import { IconExternalLink } from "@tabler/icons-react";
+
+import { SectionTitle, Showcase } from "@shared/components/commons";
+import {
+  ArrowRight,
+  Bullseye,
+  Earth,
+  Emoji,
+  Leaf,
+  SmilingEyes,
+  SparklingHeart,
+} from "@shared/styles/emoji";
 
 export default function Page() {
   return (
     <Container>
       <Hero />
       <AboutMe />
+      <Mission />
       <SocialLinks />
       <FeaturedNotes />
       <Highlights />
@@ -38,13 +52,20 @@ const Hero = () => (
         <Title order={2} weight={600} size={20} color="dimmed">
           Welcome to my personal website.
         </Title>
-        <Text mt={12}>
-          I'm Doruk Eray, a <b>founder</b>, <b>polymath</b> and <b>craftsman</b>{" "}
+        <Text mt={12} size="lg">
+          I'm Doruk Eray, a <b>Founder</b>, <b>Polymath</b> and <b>Craftsman</b>{" "}
           based in <b>Istanbul</b>.
         </Text>
         <Group my={16}>
-          <Button size="lg" radius="lg" rightIcon={<IconArrowRight />}>
+          <Button size="lg" radius="lg">
             Let's Work Together!
+          </Button>
+          <Button
+            size="md"
+            radius="lg"
+            variant="light"
+            rightIcon={<IconExternalLink />}>
+            Our Company
           </Button>
         </Group>
       </Stack>,
@@ -53,16 +74,92 @@ const Hero = () => (
 );
 
 const AboutMe = () => (
-  <Container>
-    <Title order={2}>About Me</Title>
-    <Text>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe dicta
-      voluptatem deleniti culpa? Eveniet alias pariatur, tempora enim sequi
-      perspiciatis tenetur obcaecati iure architecto excepturi ab. Ab neque
-      laudantium adipisci?
-    </Text>
-  </Container>
+  <Showcase
+    content={[
+      <Stack>
+        <Title order={2}>In a nutshell...</Title>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Necessitatibus mollitia architecto ea in consectetur neque at nulla
+          nobis. Error quaerat nobis exercitationem dolorum nihil
+          necessitatibus?
+        </Text>
+      </Stack>,
+      <List icon={"∗"}>
+        <List.Item>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae,
+            eius?
+          </Text>
+        </List.Item>
+      </List>,
+    ]}
+  />
 );
+
+export const Mission = () => {
+  const theme = useMantineTheme();
+
+  return (
+    <Container my={25} p={0}>
+      <Showcase
+        content={[
+          <SectionTitle
+            title="Our Purpose"
+            text="Why we do this?"
+            icon={Leaf}
+          />,
+          <Box component="section" id="our-purpose" py={20}>
+            <List spacing={8} center>
+              <List.Item icon={<Emoji size={24} emoji={SparklingHeart} />}>
+                Help people find and pursue their true purpose.
+              </List.Item>
+              <List.Item icon={<Emoji size={24} emoji={SmilingEyes} />}>
+                Create a meaningful life experience for everyone.
+              </List.Item>
+              <List.Item icon={<Emoji size={24} emoji={Earth} />}>
+                Build the utopian dream of heaven on earth.
+              </List.Item>
+            </List>
+          </Box>,
+        ]}
+      />
+
+      <Showcase
+        content={[
+          <SectionTitle
+            title="Our Mission"
+            text="Our roadmap and objectives."
+            icon={Bullseye}
+          />,
+          <Box component="section" id="our-purpose" py={20}>
+            <Box>
+              <Text
+                size={24}
+                weight={800}
+                color={theme.colorScheme == "dark" ? theme.white : theme.black}>
+                Liberate the Humankind;
+              </Text>
+              <Text size={18} weight={500} color="dimmed">
+                with meaningful technology.
+              </Text>
+            </Box>
+            <List
+              icon={<Emoji size={24} emoji={ArrowRight} />}
+              spacing={20}
+              my="md"
+              center>
+              <List.Item>
+                <Text weight={700}>Empowering the world’s best minds.</Text>
+                <Text>From students and hobbyists to startups.</Text>
+              </List.Item>
+            </List>
+          </Box>,
+        ]}
+      />
+    </Container>
+  );
+};
 
 const SocialLinks = () => <></>;
 const FeaturedNotes = () => <></>;
