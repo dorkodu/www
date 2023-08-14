8794738641e927ea0b338a5032542d9d16338096
doruk eray <doruk@dorkodu.com>
2023-08-14 23:09:00 +0300

    Update Contact.tsx

--- doruk/src/components/Contact.tsx
+++ doruk/src/components/Contact.tsx
@@ -1,27 +1,37 @@
 import { Box, Group, Paper, Text, UnstyledButton } from "@mantine/core";
 
 export type ContactInfo = {
-  platform: string;
-  tag: string;
+  title: string;
+  tag?: string;
   link: string;
-  icon: React.ReactNode;
+  icon?: React.ReactNode;
 };
 
 export const ContactInfoCard = ({ info }: { info: ContactInfo }) => {
   return (
     <Box maw={250} mx="auto">
-      <UnstyledButton component="a" href={info.link} title={info.platform}>
+      <UnstyledButton component="a" href={info.link} title={info.title}>
         <Paper withBorder p={10} radius="lg">
           <Group noWrap>
             {info.icon}
             <div>
-              <Text weight={500}>{info.platform}</Text>
+              <Text weight={500}>{info.title}</Text>
               <Text color="blue" weight={500} underline>
                 {info.tag}
               </Text>
             </div>
           </Group>
         </Paper>
+      </UnstyledButton>
+    </Box>
+  );
+};
+
+export const ContactInfoIcon = ({ info }: { info: ContactInfo }) => {
+  return (
+    <Box maw={250} mx="auto">
+      <UnstyledButton component="a" href={info.link} title={info.title}>
+        {info.icon}
       </UnstyledButton>
     </Box>
   );
