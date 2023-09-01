e7ea0fc06017a64ad80ad032568da4e5ec4925ab
doruk eray <doruk@dorkodu.com>
2023-09-01 21:37:21 +0300

    [doruk] BUILD

--- doruk/out/assets/IconArrowRight-659f1735.js
+++
@@ -1,1 +0,0 @@
-import{f as r}from"./index-1d8a79cf.js";var o=r("arrow-right","IconArrowRight",[["path",{d:"M5 12l14 0",key:"svg-0"}],["path",{d:"M13 18l6 -6",key:"svg-1"}],["path",{d:"M13 6l6 6",key:"svg-2"}]]);export{o as I};
