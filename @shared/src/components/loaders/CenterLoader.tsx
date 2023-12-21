6f0b9e837c9c8539fc4733cc3f277e89eee0d288
Doruk Eray <doruk@dorkodu.com>
2023-12-21 23:58:43 +0300

    dunno what happened here

--- @shared/src/components/loaders/CenterLoader.tsx
+++ @shared/src/components/loaders/CenterLoader.tsx
@@ -1,4 +1,5 @@
-import { CSSObject, Loader } from "@mantine/core";
+import { Loader } from "@mantine/core";
+
 import { useDelay } from "../hooks";
 
 const center = {
@@ -6,11 +7,11 @@ const center = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-} satisfies CSSObject
+};
 
 export default function CenterLoader() {
   const delay = useDelay();
   if (delay) return null;
 
-  return <Loader variant="dots" color="green" sx={center} />
+  return <Loader variant="dots" color="green" sx={center} />;
 }
