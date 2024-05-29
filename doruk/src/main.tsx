384915d1ada14ea6de23d590f6aad29fc8fa0c26
Berk Cambaz <berkcambaz12321@gmail.com>
2024-05-29 23:52:32 +0300

    Fix main import typo

--- doruk/src/main.tsx
+++ doruk/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import Router from "./Router";
+import Router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
